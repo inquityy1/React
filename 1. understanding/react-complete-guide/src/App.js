f341826b0705d63f44b50e2da2a468035c43a87b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-11 22:21:46 +0200

    understanding & using state

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -3,15 +3,24 @@ import "./App.css";
 import Person from "./Person/Person.js";
 
 function App() {
+  state = {
+    persons: [
+      { name: 'Max', age: 28  },
+      { name: 'Mnu', age: 29  }
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is realy working</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29">
+      <button>Switch Name</button>
+      <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+      <Person name={this.state.person[1].name} age={this.state.person[1].age}>
         My Hobbies: Racing
       </Person>
-      <Person name="Stephanie" age="26" />
+      <Person name={this.state.person[2].name} age={this.state.person[2].age} />
     </div>
     // <h1>Something</h1> -doesnt work
   );
