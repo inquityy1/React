07cbd30ae54b95e50ed0365c329e700a127d2527
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-11 19:13:24 +0200

    Working with Props

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -7,9 +7,11 @@ function App() {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is realy working</p>
-      <Person />
-      <Person />
-      <Person />
+      <Person name="Max" age="28" />
+      <Person name="Manu" age="29">
+        My Hobbies: Racing
+      </Person>
+      <Person name="Stephanie" age="26" />
     </div>
     // <h1>Something</h1> -doesnt work
   );
--- 1. understanding/react-complete-guide/src/Person/Person.js
+++ 1. understanding/react-complete-guide/src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const person = () => {
+const person = (props) => {
   return (
-    <p>I'm a Person and I am {Math.floor(Math.random() * 30)} year old!</p>
+    <p>
+      I'm {props.name} and I am {props.age} year old!
+    </p>
   );
 };
 
