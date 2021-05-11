438aa48cc77ae74f807d360e7425ba01ab122c61
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-11 18:55:27 +0200

    Creating a functional component

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Person from "./Person/Person.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is realy working</p>
+      <Person />
     </div>
     // <h1>Something</h1> -doesnt work
   );
