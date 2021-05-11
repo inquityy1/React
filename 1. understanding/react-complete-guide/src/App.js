34c64fa339dc389bfb2a8d68e6696b8cdde4a571
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-11 18:44:35 +0200

    JSX Restrictions

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
+      <p>This is realy working</p>
     </div>
+    // <h1>Something</h1> -doesnt work
   );
   // return React.createElement(
   //   "div",
