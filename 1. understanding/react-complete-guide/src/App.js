b28b1d49127c4be3da28f0b384b56e1c8db0379b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-11 18:36:19 +0200

    understanding JSX

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,11 @@ function App() {
       <h1>Hi, I'm a React App</h1>
     </div>
   );
+  // return React.createElement(
+  //   "div",
+  //   null,
+  //   React.createElement("h1", { className: "App" }, "Does this work now?")
+  // );
 }
 
 export default App;
