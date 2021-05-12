eeaacf230d6726d531b998c3a749729b9dc6682e
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-12 15:33:35 +0200

    i messed up and now i fixed that

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -1,67 +1,85 @@
-import React, { Component } from "react";
-import "./App.css";
-
-import UserInput from "./UserInput/UserInput";
-import UserOutput from "./UserOutput/UserOutput";
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    username: "supermax",
-  };
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  usernameChangedHandler = (event) => {
-    this.setState({ username: event.target.value });
-  };
+  switchNameHandler = ( newName ) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
 
-  render() {
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind( this, 'Max!' )}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>
-            UserInput should hold an input element, UserOutput two paragraphs
-          </li>
-          <li>
-            Output multiple UserOutput components in the App component (any
-            paragraph texts of your choice)
-          </li>
-          <li>
-            Pass a username (of your choice) to UserOutput via props and display
-            it there
-          </li>
-          <li>
-            Add state to the App component (=> the username) and pass the
-            username to the UserOutput component
-          </li>
-          <li>
-            Add a method to manipulate the state (=> an event-handler method)
-          </li>
-          <li>
-            Pass the event-handler method reference to the UserInput component
-            and bind it to the input-change event
-          </li>
-          <li>
-            Ensure that the new input entered by the user overwrites the old
-            username passed to UserOutput
-          </li>
-          <li>
-            Add two-way-binding to your input (in UserInput) to also display the
-            starting username
-          </li>
-          <li>
-            Add styling of your choice to your components/ elements in the
-            components - both with inline styles and stylesheets
-          </li>
-        </ol>
-        <UserInput
-          changed={this.usernameChangedHandler}
-          currentName={this.state.username}
-        />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName={this.state.username} />
-        <UserOutput userName="Max" />
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
--- 1. understanding/react-complete-guide/src/Person/Person.js
+++ 1. understanding/react-complete-guide/src/Person/Person.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 
-const person = (props) => {
-  return (
-    <div>
-      <p>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-    </div>
-  );
+import './Person.css';
+
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
 };
 
 export default person;
