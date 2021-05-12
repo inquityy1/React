85ae2a85343aa5871fc41fcb121209d5316cd9af
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-12 21:56:25 +0200

    settings styles dinamically and hot fix

--- 1. understanding/react-complete-guide/src/App.js
+++ 1. understanding/react-complete-guide/src/App.js
@@ -46,7 +46,8 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -68,6 +69,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
 
     return (
