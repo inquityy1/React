99d7e06b9b603829fc8bda0854dd351b3a928d0e
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-14 18:26:31 +0200

    Component update lifecycle

--- 1. understanding/react-complete-guide/src/components/Persons/Persons.js
+++ 1. understanding/react-complete-guide/src/components/Persons/Persons.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Person from './Person/Person';
+import Person from "./Person/Person";
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
-    return props.persons.map( ( person, index ) => {
-        return( 
+class Persons extends Component {
+  //   static getDerivedStateFromProps(props, state) {
+  //     console.log("[Perso.js] getDerivedStateFromProps");
+  //     return state;
+  //   }
+
+  //   componentWillReceiveProps() {
+  //     console.log("sadsadsasda");
+  //   }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shoulComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  //   componentWillUpdate() {
+
+  //   }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
         <Person
-            click={() => props.clicked( index )}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={( event ) => props.changed( event, person.id )}
-         />
-        );
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
     });
-};
+  }
+}
 
-export default persons;
+export default Persons;
