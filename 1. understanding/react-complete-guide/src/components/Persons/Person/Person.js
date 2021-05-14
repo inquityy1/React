a4f077ad191ea928cf9893aae59a12a7ad7c00a4
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-14 23:58:42 +0200

    Rendering Adjecent JSX Element

--- 1. understanding/react-complete-guide/src/components/Persons/Person/Person.js
+++ 1. understanding/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 
+import Auxiliary from "../../../hoc/Auxiliary";
 import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
+      <Auxiliary>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        <p key="i2">{this.props.children}</p>
         <input
+          key="i3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Auxiliary>
     );
   }
 }
