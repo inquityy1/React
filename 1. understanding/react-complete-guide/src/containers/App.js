2faf12bbe11dfaad198e83161b9fa185568964a5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-14 16:37:25 +0200

    splitting app.js in Cockpit folder

--- 1. understanding/react-complete-guide/src/containers/App.js
+++ 1. understanding/react-complete-guide/src/containers/App.js
@@ -1,7 +1,8 @@
-/* eslint-disable react/jsx-no-undef */
 import React, { Component } from 'react';
+
 import classes from './App.css';
 import Persons from '../components/Persons/Persons.js';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -47,36 +48,20 @@ class App extends Component {
 
   render () {
     let persons = null;
-    let btnClass = '';
 
     if ( this.state.showPersons ) {
-      persons = (
-        <div>
-          <Persons 
+      persons = <Persons 
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
             changed={this.nameChangedHandler} />
-        </div>
-      );
-
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if ( this.state.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
-    }
-    if ( this.state.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
     }
 
     return (
         <div className={classes.App}>
-          <h1>Hi, I'm a React App</h1>
-          <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-          <button
-          className={btnClass}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          <Cockpit 
+            showPersons={this.state.showPersons}
+            persons={this.state.persons} 
+            clicked={this.togglePersonsHandler}/>
           {persons}
         </div>
     );
