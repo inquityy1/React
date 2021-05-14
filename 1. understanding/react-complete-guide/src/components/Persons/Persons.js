55bab8a2936c3de1f94e02fa7800f8af859b8a12
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-14 23:17:23 +0200

    using shouldComponentUpdate for optimization

--- 1. understanding/react-complete-guide/src/components/Persons/Persons.js
+++ 1. understanding/react-complete-guide/src/components/Persons/Persons.js
@@ -14,6 +14,11 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log("[Persons.js] shouldComponentUpdate");
+    // if (nextProps.persons !== this.props.persons) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
     return true;
   }
 
