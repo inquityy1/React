f3c73b2f08014235460bc9f42c9122d8f1d2cab4
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-14 17:56:38 +0200

    component creation lifecycle in action

--- 1. understanding/react-complete-guide/src/containers/App.js
+++ 1. understanding/react-complete-guide/src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons.js';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -14,6 +19,19 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false
   }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }; 
 
   nameChangedHandler = ( event, id ) => {
     const personIndex = this.state.persons.findIndex( p => {
@@ -47,6 +65,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] render');
     let persons = null;
 
     if ( this.state.showPersons ) {
@@ -59,6 +78,7 @@ class App extends Component {
     return (
         <div className={classes.App}>
           <Cockpit 
+            title={this.props.appTitle}
             showPersons={this.state.showPersons}
             persons={this.state.persons} 
             clicked={this.togglePersonsHandler}/>
