a0a4b205afffc4685423d61c2dcdf68fdeea639f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-18 00:27:50 +0200

    Adding a Dynamic Ingredient Component

--- 2. Burger app/burger-app/src/components/Layout/Layout.js
+++ 2. Burger app/burger-app/src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import classes from "./Layout.css";
 const layout = (props) => (
   <Auxiliary>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
+    <main className={"Content"}>{props.children}</main>
   </Auxiliary>
 );
 
