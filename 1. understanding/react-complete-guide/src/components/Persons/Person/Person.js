e342ce65b992ccae4a8df709b6712b5eb72c3a56
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-17 05:16:11 +0200

    Using Refs

--- 1. understanding/react-complete-guide/src/components/Persons/Person/Person.js
+++ 1. understanding/react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,6 +6,16 @@ import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering...");
     return (
@@ -16,6 +26,10 @@ class Person extends Component {
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
+          // ref={(inputEl) => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
