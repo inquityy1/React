a61b075e3b4357bf423a9477b394d48f01953721
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-18 04:20:54 +0200

    Connecting State to Build Controls

--- 2. Burger app/burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ 2. Burger app/burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,9 @@ const buildControl = (props) => (
   <div className={"BuildControl"}>
     <div className={"Label"}>{props.label}</div>
     <button className={"Less"}>Less</button>
-    <button className={"More"}>More</button>
+    <button className={"More"} onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
