6eef29ee387db667ddf4ea2a083dfdc9c35efb56
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-20 23:17:40 +0200

    Posting Data to the Server

--- 3. http/http--01-starting-setup/src/containers/Blog/Blog.js
+++ 3. http/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,6 +9,7 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
@@ -21,20 +22,31 @@ class Blog extends Component {
         };
       });
       this.setState({ posts: updatedPosts });
-      // console.log(response);
+      // console.log( response );
     });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
