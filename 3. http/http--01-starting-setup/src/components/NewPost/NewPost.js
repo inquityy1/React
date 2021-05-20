3b01fee23ace463406da409491191fbdd08407a5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-05-20 23:36:56 +0200

    Sending a Delete Request + Hotfix

--- 3. http/http--01-starting-setup/src/components/NewPost/NewPost.js
+++ 3. http/http--01-starting-setup/src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./NewPost.css";
 
@@ -7,6 +8,19 @@ class NewPost extends Component {
     title: "",
     content: "",
     author: "Max",
+  };
+
+  postDataHandler = () => {
+    const data = {
+      title: this.state.title,
+      body: this.state.content,
+      author: this.state.author,
+    };
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", data)
+      .then((response) => {
+        console.log(response);
+      });
   };
 
   render() {
@@ -33,7 +47,7 @@ class NewPost extends Component {
           <option value="Max">Max</option>
           <option value="Manu">Manu</option>
         </select>
-        <button>Add Post</button>
+        <button onClick={this.postDataHandler}>Add Post</button>
       </div>
     );
   }
