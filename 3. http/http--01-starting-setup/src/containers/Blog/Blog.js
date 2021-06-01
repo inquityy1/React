6e87671611ab47ff1d439cac2ceaa455295fafae
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-06-01 17:02:00 +0200

    Absolute vs Relative Paths

--- 3. http/http--01-starting-setup/src/containers/Blog/Blog.js
+++ 3. http/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -19,7 +19,7 @@ class Blog extends Component {
               <li>
                 <Link
                   to={{
-                    pathname: "/new-post",
+                    pathname: this.props.match.url + "/new-post",
                     hash: "#submit",
                     search: "?quick-submit=true",
                   }}
