bb9993abfa49a9989ab698d31ace2b6868eabca8
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-06-02 17:15:50 +0200

    Loading Routes Lazily

--- 3. http/http--01-starting-setup/src/containers/Blog/Blog.js
+++ 3. http/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent";
+// import NewPost from "./NewPost/NewPost";
+
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
   state = {
-    auth: false,
+    auth: true,
   };
 
   render() {
@@ -48,10 +53,11 @@ class Blog extends Component {
         <Route path="/" exact render={() => <h1>Home 2</h1>} /> */}
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" component={NewPost} />
+            <Route path="/new-post" component={AsyncNewPost} />
           ) : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>Not found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
           {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
