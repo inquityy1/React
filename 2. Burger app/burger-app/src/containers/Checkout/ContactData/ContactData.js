09c6e2ee825ec153aff1fece0e6bc2242739705a
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-06-05 01:31:17 +0200

    Handling Overall Form Validity

--- 2. Burger app/burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ 2. Burger app/burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,9 @@ class ContactData extends Component {
             { value: "cheapest", displayValue: "Cheapest" },
           ],
         },
+        formIsValid: false,
         value: "",
+        valid: true,
       },
     },
     loading: false,
@@ -146,8 +148,13 @@ class ContactData extends Component {
     );
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    console.log(formIsValid);
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -172,7 +179,7 @@ class ContactData extends Component {
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
           ORDER
         </Button>
       </form>
