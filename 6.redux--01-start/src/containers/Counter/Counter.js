3a45c2ee54fbc5ea2497928d36d2adf38ac9e6ce
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-06-09 11:59:30 +0200

    Updating Arrays Immutably and Update state Immutably

--- 6.redux--01-start/src/containers/Counter/Counter.js
+++ 6.redux--01-start/src/containers/Counter/Counter.js
@@ -44,16 +44,28 @@ class Counter extends Component {
         />
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
+          clicked={() => this.props.onDecrementCounter}
         />
         <CounterControl
           label="Add 10"
-          clicked={() => this.counterChangedHandler("add", 5)}
+          clicked={() => this.props.onAddCounter}
         />
         <CounterControl
           label="Subtract 15"
-          clicked={() => this.counterChangedHandler("sub", 5)}
+          clicked={() => this.props.onSubtractCounter}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map((strResult) => (
+            <li
+              key={strResult.id}
+              onClick={() => this.props.onDeleteResult(strResult.id)}
+            >
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -62,15 +74,18 @@ class Counter extends Component {
 const mapStateToProps = (state) => {
   return {
     ctr: state.counter,
+    storedResults: state.results,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
-    onDecrementCounterCounter: () => dispatch({ type: "DECREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
     onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: (id) => dispatch({ type: "DELETE_RESULT", resultElId: id }),
   };
 };
 
--- 6.redux--01-start/src/store/reducer.js
+++ 6.redux--01-start/src/store/reducer.js
@@ -1,24 +1,44 @@
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return {
-        counter: state.counter + 1,
-      };
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
     case "DECREMENT":
       return {
+        ...state,
         counter: state.counter - 1,
       };
     case "ADD":
       return {
+        ...state,
         counter: state.counter + action.val,
       };
     case "SUBTRACT":
       return {
+        ...state,
         counter: state.counter - action.val,
+      };
+    case "STORE_RESULT":
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: state.counter }),
+      };
+    case "DELETE_RESULT":
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+      const updatedArray = state.results.filter(
+        (result) => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray,
       };
   }
   return state;
