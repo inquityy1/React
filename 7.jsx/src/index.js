aea9c519e6025853300f912113532f2af65f5577
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-06-25 00:07:47 +0200

    Class vs ClassName

--- 7.jsx/src/index.js
+++ 7.jsx/src/index.js
@@ -2,11 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// class App extends React.Component
+
 // Create a react component
 const App = () => {
   return (
     <div>
-      <label class="label">Enter name:</label>
+      <label className="label">Enter name:</label>
       <input id="name" type="text " />
       {/* <button style="background-color: blue; color: white;">Submit</button> */}
       <button style={{ backgroundColor: "blue", color: "white" }}>
