e71f7056f06e7ca323c78d54efa1cfaffb2a7921
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-06-29 19:13:48 +0200

    Component Nesting

--- 8.components/src/index.js
+++ 8.components/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 
+import CommentDetail from "./CommentDetail";
+
 const App = () => {
-  return <div className="ui container comments"></div>;
+  return (
+    <div className="ui container comments">
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
