5394b7d7930e99c2598dce85beafb5ceee932a91
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-01 16:43:00 +0200

    Class Based Component

--- 9.seasons/src/index.js
+++ 9.seasons/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
 
-  return <div>Hi there!</div>;
-};
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
