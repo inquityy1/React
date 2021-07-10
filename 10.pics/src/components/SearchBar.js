29bc51d8f48c39ab4ae29a60dc719dc93cb87f54
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-10 09:50:41 +0200

    Showing Forms to the User

---
+++ 10.pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="Type" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
