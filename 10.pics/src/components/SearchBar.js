07fd1dd31b70c97ae095c2a42180a0fee19fce4c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-10 09:59:09 +0200

    Adding a Touch of Style

--- 10.pics/src/components/SearchBar.js
+++ 10.pics/src/components/SearchBar.js
@@ -3,9 +3,12 @@ import React from "react";
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" placeholder="Type" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" placeholder="Type" />
+          </div>
         </form>
       </div>
     );
