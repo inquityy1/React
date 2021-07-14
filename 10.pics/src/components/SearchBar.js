4f88f3e747dc62c3ba3da2eea03fe73899cedfc4
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-14 20:26:00 +0200

    Handling Form Submittal

--- 10.pics/src/components/SearchBar.js
+++ 10.pics/src/components/SearchBar.js
@@ -3,17 +3,23 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: '' }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input 
               type="text" 
               value={this.state.term} 
               onChange={(e) =>
-                 this.setState({term: e.target.value.toUpperCase()})
+                 this.setState({term: e.target.value})
               }
             />
           </div>
