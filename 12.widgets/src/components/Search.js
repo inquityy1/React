a467ddb6fcbe2fd176e1347d21c5d57d337919d7
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-28 00:45:32 +0200

    Text Inputs with Hooks

--- 12.widgets/src/components/Search.js
+++ 12.widgets/src/components/Search.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
