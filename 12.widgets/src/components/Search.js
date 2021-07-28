b971c48b60ae58bcb8542414ac4ca55d6e1dfe0d
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-28 03:43:53 +0200

    Linking to a Wikipedia Page

--- 12.widgets/src/components/Search.js
+++ 12.widgets/src/components/Search.js
@@ -26,6 +26,14 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
