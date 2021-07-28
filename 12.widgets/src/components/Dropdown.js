5778db1a1ff446b06aa8fcbcd421f64678131043
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-28 23:29:25 +0200

    Which Element Was Clicked

--- 12.widgets/src/components/Dropdown.js
+++ 12.widgets/src/components/Dropdown.js
@@ -6,8 +6,8 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   useEffect(() => {
     document.body.addEventListener(
       "click",
-      () => {
-        console.log("BODY CLICKED");
+      (event) => {
+        console.log(event.target);
         setOpen(false);
       },
       { capture: true }
@@ -23,10 +23,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div
         key={option.value}
         className="item"
-        onClick={() => {
-          console.log("ITEM CLICKED");
-          onSelectedChange(option);
-        }}
+        onClick={() => onSelectedChange(option)}
       >
         {option.label}
       </div>
@@ -38,10 +35,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div className="field">
         <label className="label">Select a Color</label>
         <div
-          onClick={() => {
-            console.log("DROPDOWN CLICKED");
-            setOpen(!open);
-          }}
+          onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
         >
           <i className="dropdown icon"></i>
