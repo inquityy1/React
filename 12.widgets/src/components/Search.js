4f722b1426d6c910d70935ff8bdaf072675a727f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-28 04:23:49 +0200

    Implementing a Delayed Request

--- 12.widgets/src/components/Search.js
+++ 12.widgets/src/components/Search.js
@@ -6,34 +6,30 @@ const Search = () => {
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log("initial render");
+    const search = async () => {
+      const { data } = await axios.get("http://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 1000);
 
     return () => {
-      console.log("CLEANUP");
+      clearTimeout(timeoutId);
     };
   }, [term]);
-
-  //   useEffect(() => {
-  //     const search = async () => {
-  //       const { data } = await axios.get("http://en.wikipedia.org/w/api.php", {
-  //         params: {
-  //           action: "query",
-  //           list: "search",
-  //           origin: "*",
-  //           format: "json",
-  //           srsearch: term,
-  //         },
-  //       });
-
-  //       setResults(data.query.search);
-  //     };
-
-  //     const timeoutId = setTimeout(() => {
-  //       if (term) {
-  //         search();
-  //       }
-  //     }, 500);
-  //   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
