a8de5685a8f216b71a616580d937a301d479f215
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-28 22:36:26 +0200

    Filtering the Option List

--- 12.widgets/src/components/Dropdown.js
+++ 12.widgets/src/components/Dropdown.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
       <div
         key={option.value}
