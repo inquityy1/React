c4dff28152ce3cffc7722fe86200c16b1cabaf13
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-30 00:21:01 +0200

    Detecting Navigation

--- 12.widgets/src/components/Route.js
+++ 12.widgets/src/components/Route.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("Location Change");
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+
   return window.location.pathname === path ? children : null;
 };
 
