f5b61edd2edff571768a3aa3a81a8ac4f0c3a033
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-29 23:49:34 +0200

    Building a Reusable Route Component

--- 12.widgets/src/App.js
+++ 12.widgets/src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
   {
@@ -59,12 +60,26 @@ const showTranslate = () => {
 };
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
