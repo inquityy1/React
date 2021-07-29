1b3c1567c78a10ec1f0cde75cfd3c53220eb936b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-07-29 02:53:45 +0200

    Scaffolding the Translate Component

--- 12.widgets/src/App.js
+++ 12.widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -34,15 +35,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-
   return (
     <div>
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <Translate />
     </div>
   );
 };
--- 12.widgets/src/components/Dropdown.js
+++ 12.widgets/src/components/Dropdown.js
@@ -5,13 +5,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener("click", (event) => {
-      if (ref.current.contains(event.target)) {
+    const onBodyClick = (event) => {
+      if (ref.current && ref.current.contains(event.target)) {
         return;
       }
 
       setOpen(false);
-    });
+    };
+    document.body.addEventListener("click", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
   }, []);
 
   const renderedOptions = options.map((option) => {
