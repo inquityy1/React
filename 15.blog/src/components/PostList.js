853497e0fa8a0a92476772b8f8542a87584ebfdd
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-04 12:47:29 +0200

    Displaying the User Header

--- 15.blog/src/components/PostList.js
+++ 15.blog/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
@@ -10,13 +11,14 @@ class PostList extends React.Component {
   renderList() {
     return this.props.posts.map((post) => {
       return (
-        <div className="item" key={post}>
+        <div className="item" key={post.id}>
           <i className="large middle aligned icon user"></i>
           <div className="content">
             <div className="description">
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
