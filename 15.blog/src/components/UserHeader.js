c59e5a523c3952d3347dd190bd9f870c22b20209
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-04 20:55:08 +0200

    Extracting Logic to MapStateToProps

--- 15.blog/src/components/UserHeader.js
+++ 15.blog/src/components/UserHeader.js
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
 
     if (!user) {
       return <div>Loading...</div>;
@@ -18,8 +18,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
