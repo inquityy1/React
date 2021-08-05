c3039ab0bbffb716cc4665fb58ded992878c2be3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-05 14:01:57 +0200

    Action Creators in Action Creators

--- 15.blog/src/actions/index.js
+++ 15.blog/src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  console.log("About to fetch posts");
+  await dispatch(fetchPosts());
+  console.log("Fetch posts!");
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
