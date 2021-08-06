37b83525fecefadf16787f6adb6780aa985fe9dc
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-06 02:22:17 +0200

    MemoryRouter

--- 16.streams/client/src/components/App.js
+++ 16.streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { MemoryRouter, Route, Link } from "react-router-dom";
 
 const PageOne = () => {
   return (
@@ -24,12 +24,12 @@ const PageTwo = () => {
 const App = () => {
   return (
     <div>
-      <HashRouter>
+      <MemoryRouter>
         <div>
           <Route path="/" exact={true} component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
         </div>
-      </HashRouter>
+      </MemoryRouter>
     </div>
   );
 };
