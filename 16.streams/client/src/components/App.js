a98445eebdfbd9b4679a7f6373de3dfb5cda7e1b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-06 02:11:20 +0200

    How to Not Navigate with React Router

--- 16.streams/client/src/components/App.js
+++ 16.streams/client/src/components/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne BAD!!!
+      <a href="/pagetwo">Navigate to Page Two</a>
+    </div>
+  );
 };
 
 const PageTwo = () => {
@@ -10,6 +15,8 @@ const PageTwo = () => {
     <div>
       PageTwo
       <button>Click Me!</button>
+      BAD!!!
+      <a href="/">Navigate to Page One</a>
     </div>
   );
 };
@@ -19,7 +26,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
+          <Route path="/" exact={true} component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
         </div>
       </BrowserRouter>
