67f9837deb6fad332329c7d1e6b7a811337b937a
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-25 00:39:20 +0200

    Wiring Up the Google API Library

---
+++ 16.streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "1067036875913-mb29hfnoojnuu393rmcdu8plna6eotrt.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
