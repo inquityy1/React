1dab6241caafbba29675737afccb741bc1ad79d4
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-08-26 03:00:16 +0200

    Displaying Validation Messages

--- 16.streams/client/src/components/streams/StreamCreate.js
+++ 16.streams/client/src/components/streams/StreamCreate.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
@@ -49,4 +50,5 @@ const validate = (formValues) => {
 
 export default reduxForm({
   form: "streamCreate",
+  validate: validate,
 })(StreamCreate);
