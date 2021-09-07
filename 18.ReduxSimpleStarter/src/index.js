57311d469174ecf383215244f153c3ea5160916e
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-07 20:12:07 +0200

    Export Statements

--- 18.ReduxSimpleStarter/src/index.js
+++ 18.ReduxSimpleStarter/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchBar from "./components/search_bar";
+
 const API_KEY = "AIzaSyDHQKsEAwSqW903HC-wJMlkXZl9opBVEmI";
 
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put it
