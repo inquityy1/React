aa6faa8fb9dabc402f5174d43fa2ebf6eb3c6dde
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-07 22:35:29 +0200

    Props

--- 18.ReduxSimpleStarter/src/index.js
+++ 18.ReduxSimpleStarter/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 const API_KEY = "AIzaSyDHQKsEAwSqW903HC-wJMlkXZl9opBVEmI";
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
