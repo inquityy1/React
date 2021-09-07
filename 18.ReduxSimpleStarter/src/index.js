aef9ab2dd3861fef81c8ff2e61a166e9ef235071
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-07 22:15:40 +0200

    Youtube Search Response

--- 18.ReduxSimpleStarter/src/index.js
+++ 18.ReduxSimpleStarter/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyDHQKsEAwSqW903HC-wJMlkXZl9opBVEmI";
+
+YTSearch({ key: API_KEY, term: "surfboards" }, function (data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce
 // some HTML
