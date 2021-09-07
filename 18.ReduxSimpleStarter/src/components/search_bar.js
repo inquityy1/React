0654f990c4a12f332015f3528688d405b261e607
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-07 21:54:08 +0200

    Controlled Components

--- 18.ReduxSimpleStarter/src/components/search_bar.js
+++ 18.ReduxSimpleStarter/src/components/search_bar.js
@@ -4,13 +4,14 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: "" };
+    this.state = { term: "Starting Value" };
   }
 
   render() {
     return (
       <div>
         <input
+          value={this.state.term}
           onChange={(event) => this.setState({ term: event.target.value })}
         />
       </div>
