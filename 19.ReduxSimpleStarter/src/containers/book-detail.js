fc0f3244cae46ffa462208f84c9cf15fbcd62938
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-09 00:53:44 +0200

    Conditional Rendering

--- 19.ReduxSimpleStarter/src/containers/book-detail.js
+++ 19.ReduxSimpleStarter/src/containers/book-detail.js
@@ -3,7 +3,17 @@ import { connect } from "react-redux";
 
 class BookDetail extends Component {
   render() {
-    return <div>Book Detail!</div>;
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
+    return (
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
+    );
   }
 }
 
