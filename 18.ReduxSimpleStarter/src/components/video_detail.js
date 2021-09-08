22b77e0e52aab589216dd271e460a3486dce4da7
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-08 19:01:28 +0200

    hotfix

--- 18.ReduxSimpleStarter/src/components/video_detail.js
+++ 18.ReduxSimpleStarter/src/components/video_detail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
 
   return (
     <div className="video-detail col-md-8">
-      <div className="embed-responsive emved-responsive-16by9">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
 
