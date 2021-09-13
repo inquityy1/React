01e7fbac7d22317f6eef48c2b0c78475dbc8080f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-13 12:56:03 +0200

    Component Setup

--- 19.ReduxSimpleStarter(Books)/src/actions/index.js
+++ 19.ReduxSimpleStarter(Books)/src/actions/index.js
@@ -1,7 +1,6 @@
 export function selectBook(book) {
   // selectBook is an ActionCreator, it needs to return an action,
   // an object with a type property.
-
   return {
     type: "BOOK_SELECTED",
     payload: book,
