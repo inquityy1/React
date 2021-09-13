a2e56c9db64393b9c6e906dc7c95d9393aa1ce23
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-13 14:11:47 +0200

    Ajax Request with Axios

--- 20.ReduxSimpleStarter(Weather)/src/actions/index.js
+++ 20.ReduxSimpleStarter(Weather)/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from "axios";
+
 const API_KEY = "4aa1c017a192034ab732f39d053b301e";
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
