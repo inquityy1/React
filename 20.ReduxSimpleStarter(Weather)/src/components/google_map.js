35a1691b7638cde39a515274b7f33e755694d883
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-14 20:58:12 +0200

    Google Maps Integration done

--- 20.ReduxSimpleStarter(Weather)/src/components/google_map.js
+++ 20.ReduxSimpleStarter(Weather)/src/components/google_map.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class GoogleMap extends Component {
   componentDidMount() {
-    new GoogleMap.maps.Map(this.refs.map, {
+    new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
         lat: this.props.lat,
