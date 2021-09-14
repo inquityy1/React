f47adf504048b86efcfc285c81bcff867c0c22f3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-14 18:39:16 +0200

    Mapping Props to a Render Helper

--- 20.ReduxSimpleStarter(Weather)/src/containers/weather_list.js
+++ 20.ReduxSimpleStarter(Weather)/src/containers/weather_list.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -13,7 +23,7 @@ class WeatherList extends Component {
             <th>Humidity</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
