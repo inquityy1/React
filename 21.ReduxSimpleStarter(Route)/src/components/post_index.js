865cca602ae99be9854710d393f387c295052f26
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 01:17:58 +0200

    Action Creator Shortcuts

--- 21.ReduxSimpleStarter(Route)/src/components/post_index.js
+++ 21.ReduxSimpleStarter(Route)/src/components/post_index.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
-        <div></div>
-        Posts Index
+        <div>Posts Index</div>
       </div>
     );
   }
 }
 
-export default PostIndex;
+export default connect(null, { fetchPosts })(PostIndex);
