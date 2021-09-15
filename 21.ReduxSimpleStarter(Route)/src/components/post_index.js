0951d0b0f98784adda9e1ee3b02c2b862eb76a27
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-15 23:28:55 +0200

    Back to Redux - index Action

---
+++ 21.ReduxSimpleStarter(Route)/src/components/post_index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class PostIndex extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+        Posts Index
+      </div>
+    );
+  }
+}
+
+export default PostIndex;
