1accb52b2975c30552f80f68f320d316aa75741c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 02:07:34 +0200

    Rendering a List of Posts

--- 21.ReduxSimpleStarter(Route)/src/components/post_index.js
+++ 21.ReduxSimpleStarter(Route)/src/components/post_index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
@@ -7,13 +8,28 @@ class PostIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    _.map(this.props.posts, (post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <div>Posts Index</div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostIndex);
+function mapStateToProps(state) {
+  return { posts: state.props };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
