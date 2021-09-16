6299d444ca7c3ddb9032753b17d9502daf4f4454
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 17:09:24 +0200

    The Field Component

--- 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
+++ 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input type="text" {...field.input} />
+      </div>
+    );
+  }
+
   render() {
     return (
       <form>
-        <Field
-         name="title"
-          component={}
-        />
+        <Field name="title" component={this.renderTitleField} />
       </form>
     );
   }
