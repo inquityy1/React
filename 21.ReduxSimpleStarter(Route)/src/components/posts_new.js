34a2d96c229e2437d7a7de9b58a9d26192a5ff49
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 02:14:24 +0200

    Creating New Posts

---
+++ 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        <div>PostsNew</div>
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
