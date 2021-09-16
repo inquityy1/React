df8d6813aa502c5ce165c0ea4522d32b0e83f417
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 18:40:16 +0200

    More on Navigation

--- 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
+++ 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
 class PostsNew extends Component {
   renderField(field) {
@@ -45,6 +46,9 @@ class PostsNew extends Component {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
