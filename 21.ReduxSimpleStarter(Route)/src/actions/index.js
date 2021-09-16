484d9735eda9e8e69e263c68913902905230ee9b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 20:30:59 +0200

    Create Post Action Creator

--- 21.ReduxSimpleStarter(Route)/src/actions/index.js
+++ 21.ReduxSimpleStarter(Route)/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
+export const CREATE_POST = "create_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=UNGABUNGA23";
@@ -9,6 +10,15 @@ export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type: FETCH_POSTS,
+    payload: request,
+  };
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
     payload: request,
   };
 }
--- 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
+++ 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions";
 
 class PostsNew extends Component {
   renderField(field) {
@@ -19,8 +21,7 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    // this === component
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -77,4 +78,4 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: "PostsNewForm",
-})(PostsNew);
+})(connect(null, { createPost })(PostsNew));
