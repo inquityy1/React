4c0c50b06cf40b6c915dbc90dd4647ae5673bfe8
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-09-16 18:16:16 +0200

    Handling Form Submittal

--- 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
+++ 21.ReduxSimpleStarter(Route)/src/components/posts_new.js
@@ -12,9 +12,16 @@ class PostsNew extends Component {
     );
   }
 
+  onSubmit(values) {
+    // this === component
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title For Post"
           name="title"
@@ -30,6 +37,9 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     );
   }
