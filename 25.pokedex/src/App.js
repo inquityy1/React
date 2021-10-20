5965ed485604671348d19a5c885aac0df78738d0
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-20 23:50:35 +0200

    PokeGame Component

--- 25.pokedex/src/App.js
+++ 25.pokedex/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import Pokedex from "./Pokedex";
-import Pokecard from "./Pokecard";
+import Pokegame from "./Pokegame";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
