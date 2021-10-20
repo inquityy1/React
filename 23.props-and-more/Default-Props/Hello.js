9ab17b2b22ca6ce07a34aa77b09385adf89fedbf
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-20 14:07:04 +0200

    Styling React

--- 23.props-and-more/Default-Props/Hello.js
+++ 23.props-and-more/Default-Props/Hello.js
@@ -5,13 +5,18 @@ class Hello extends React.Component {
   };
 
   render() {
+    const colors = { fontSize: "50px", backgroundColor: "purple" };
     let bangs = "!".repeat(this.props.bangs);
+
     return (
-      <div>
-        <p>
+      <div className="Hello">
+        <p style={colors}>
           Hi {this.props.to} from {this.props.from}
           {bangs}
         </p>
+        {/* <p className={winner ? "win" : "lose"}>
+          {winner ? "Winner!" : "Loser!"}
+        </p> */}
       </div>
     );
   }
