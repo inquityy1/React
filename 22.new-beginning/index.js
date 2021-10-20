f5c5167e53a6ce22d64b5415641b04d93216fc1e
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-20 11:06:24 +0200

    Class and Function based components

--- 22.new-beginning/index.js
+++ 22.new-beginning/index.js
@@ -1,11 +1,21 @@
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello there!</h1>
-      </div>
-    );
-  }
+// class Hello extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello there!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+    </div>
+  );
 }
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
