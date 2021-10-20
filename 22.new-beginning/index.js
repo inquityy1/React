33c430155441e29850bae796590d5062ad071981
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-20 10:55:56 +0200

    implementing simple code

---
+++ 22.new-beginning/index.js
@@ -0,0 +1,11 @@
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello there!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
