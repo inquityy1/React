e6a2b99d3ac05d7058adf9b6c8f6077d126e141c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-20 11:11:56 +0200

    Basic Rules of JSX

--- 22.new-beginning/index.js
+++ 22.new-beginning/index.js
@@ -1,21 +1,13 @@
-// class Hello extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello there!</h1>
-//       </div>
-//     );
-//   }
-// }
-
-function Hello() {
-  return (
-    <div>
-      <h1>Hello there!</h1>
-      <h1>Hello there!</h1>
-      <h1>Hello there!</h1>
-    </div>
-  );
+class JSXDemo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My Image!</h1>
+        <img src="https://images.unsplash.com/photo-1634703080363-98f94e5a1076?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=725&q=80" />
+        ;
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+ReactDOM.render(<JSXDemo />, document.getElementById("root"));
