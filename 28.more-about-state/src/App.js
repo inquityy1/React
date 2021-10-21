6f16f49e0e46dcf55b2c5347c5fefa3bde7d4864
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-21 23:47:09 +0200

    State Design Example Lottery Component

--- 28.more-about-state/src/App.js
+++ 28.more-about-state/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import ScoreKeeper from "./ScoreKeeper";
 import Ball from "./Ball";
+import Lottery from "./Lottery";
 
 import "./App.css";
 
@@ -8,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ball num={17} />
+        <Lottery />
+        <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
       </div>
     );
   }
