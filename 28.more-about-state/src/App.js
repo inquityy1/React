0212b631bdbe85809348301a247692e574f1c362
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-21 21:37:13 +0200

    State Design Example LottoBall Component

--- 28.more-about-state/src/App.js
+++ 28.more-about-state/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ScoreKeeper from "./ScoreKeeper";
+// import ScoreKeeper from "./ScoreKeeper";
+import Ball from "./Ball";
 
 import "./App.css";
 
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ScoreKeeper />
+        <Ball num={17} />
       </div>
     );
   }
