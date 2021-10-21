8ba287a4c613686e40cca4870efd1c8054969462
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-21 11:40:02 +0200

    Alternate Syntax for state

--- 26.state/src/App.js
+++ 26.state/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Game from "./Game";
+import Game2 from "./Game2";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Game />
+        <Game2 />
       </div>
     );
   }
