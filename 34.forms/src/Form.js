daa7ae0ae462c23e4d47e79ead612fe00a48be45
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-25 19:28:27 +0200

    htmlFor Attribute

--- 34.forms/src/Form.js
+++ 34.forms/src/Form.js
@@ -21,8 +21,10 @@ class Form extends Component {
       <div>
         <h1>Form Demo</h1>
         <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username"></label>
           <input
             type="text"
+            id="username"
             value={this.state.username}
             onChange={this.handleChange}
           ></input>
