ca8b6d94d5aa97c76e68746c4c56c1fdf0567aba
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-25 18:31:37 +0200

    Simple handling forms

---
+++ 34.forms/src/Form.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt) {
+    this.setState({ username: evt.target.value });
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+    alert(`You typed: ${this.state.username}`);
+    this.setState({ username: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Form Demo</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleChange}
+          ></input>
+          <button>Submit!</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
