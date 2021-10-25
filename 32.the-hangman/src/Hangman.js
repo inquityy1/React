f4beafe44f76f80fc4a37366a8c3a8612ddb2164
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-25 13:12:25 +0200

    Styling and Wing/Lose Mechanic

--- 32.the-hangman/src/Hangman.js
+++ 32.the-hangman/src/Hangman.js
@@ -70,7 +70,11 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     const gameOver = this.state.nWrong >= this.props.maxWrong;
+    const isWinner = this.guessedWord().join("") === this.state.answer;
     const altText = `${this.state.nWrong}/${this.props.maxWrong} guesses`;
+    let gameState = this.generateButtons();
+    if (isWinner) gameState = "You Win";
+    if (gameOver) gameState = "You Lose";
     return (
       <div className="Hangman">
         <h1>Hangman</h1>
@@ -79,11 +83,11 @@ class Hangman extends Component {
         <p className="Hangman-word">
           {!gameOver ? this.guessedWord() : this.state.answer}
         </p>
-        <p className="Hangman-btns">
-          {!gameOver ? this.generateButtons() : "You lose"}
-        </p>
-        <p>{altText}</p>
-        <button onClick={this.reset}>Restart?</button>
+        <p className="Hangman-btns">{gameState}</p>
+        <p>{altText} wrong</p>
+        <button id="reset" onClick={this.reset}>
+          Restart?
+        </button>
       </div>
     );
   }
