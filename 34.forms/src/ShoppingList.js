d2076fdbb05f4936b577df2d67d0a7e26fc3bc48
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-25 22:41:47 +0200

    Using the UUID Library

--- 34.forms/src/ShoppingList.js
+++ 34.forms/src/ShoppingList.js
@@ -1,27 +1,29 @@
 import React, { Component } from "react";
 import ShoppingListForm from "./ShoppingListForm";
+import { v4 as uuidv4 } from "uuid";
 
 class ShoppingList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [
-        { name: "Milk", qty: "2 gallons" },
-        { name: "Bread", qty: "2 loaves" },
+        { name: "Milk", qty: "2 gallons", id: uuidv4() },
+        { name: "Bread", qty: "2 loaves", id: uuidv4() },
       ],
     };
     this.addItem = this.addItem.bind(this);
   }
   addItem(item) {
+    let newItem = { ...item, id: uuidv4() };
     this.setState((state) => ({
-      items: [...state.items, item],
+      items: [...state.items, newItem],
     }));
   }
   renderItems() {
     return (
       <ul>
         {this.state.items.map((item) => (
-          <li>
+          <li key={item.id}>
             {item.name}:{item.qty}
           </li>
         ))}
