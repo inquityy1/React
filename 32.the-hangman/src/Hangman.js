4baa6fdfabd3239326f3f8c93f819be8f2496770
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-25 11:58:11 +0200

    unfinished hangman game

--- 32.the-hangman/src/Hangman.js
+++ 32.the-hangman/src/Hangman.js
@@ -1,27 +1,69 @@
 import React, { Component } from "react";
+import img0 from "./img0.png";
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+import img3 from "./img3.png";
+import img4 from "./img4.png";
+import img5 from "./img5.png";
+import img6 from "./img6.png";
+import img7 from "./img7.png";
 
 import "./Hangman.css";
 
 class Hangman extends Component {
   static defaultProps = {
     maxWrong: 6,
-    images: [""],
+    images: [img0, img1, img2, img3, img4, img5, img6, img7],
   };
 
   constructor(props) {
     super(props);
     this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
-    // this.handleGuess = this.handleGuess.bind(this);
+    this.handleGuess = this.handleGuess.bind(this);
   }
 
   /** guessedWord: show current-state of word:
    * if guessed letters are {a,p,e}, show "app_e" for "apple"
    */
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map((ltr) => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
 
+  /** handleGuest: handle a guessed letter:
+      - add to guessed letters
+      - if not in answer, increase number-wrong guesses
+   */
+  handleGuess(evt) {
+    let ltr = evt.target.value;
+    this.setState((st) => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+    }));
+  }
+
+  /** generateButtons: return array of latter buttons to render */
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr) => (
+      <button
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+
+  /** render: render game */
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="Hangman">
+        <h1>Hangman</h1>
+        <img src={this.props.images[this.state.nWrong]} />
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
       </div>
     );
   }
