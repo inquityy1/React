b1935f6ded4f1e31113f4444c43003537b684f6b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-26 14:24:47 +0200

    Adding NewTodoForm

--- 36.todo/src/TodoList.js
+++ 36.todo/src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import NewTodoForm from "./NewTodoForm";
 import Todo from "./Todo";
 
 class TodoList extends Component {
@@ -7,14 +8,24 @@ class TodoList extends Component {
     this.state = {
       todos: [{ task: "Walk the Fish" }, { task: "Groom Chickens" }],
     };
+    this.create = this.create.bind(this);
   }
+
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  }
+
   render() {
     const todos = this.state.todos.map((todo) => {
       return <Todo task={todo.task} />;
     });
+
     return (
       <div>
         <h1>Todo List!</h1>
+        <NewTodoForm createTodo={this.create} />
         <ul>{todos}</ul>
       </div>
     );
