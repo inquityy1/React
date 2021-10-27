fa03dd5eabc16eb2d9dc02f0c14928f33327d895
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-27 16:58:35 +0200

    Loading Data Via AJAX

--- 38.lifecycle-methods/src/App.js
+++ 38.lifecycle-methods/src/App.js
@@ -1,11 +1,12 @@
 import Timer from "./Timer";
+import ZenQuote from "./ZenQuote";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <ZenQuote />
     </div>
   );
 }
