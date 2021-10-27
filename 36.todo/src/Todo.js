90bf768b9b4b730b5ed54225ab36f9bc7baf854c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-27 19:23:04 +0200

    PrevProps and PrevState in ComponentDid

--- 36.todo/src/Todo.js
+++ 36.todo/src/Todo.js
@@ -15,6 +15,12 @@ class Todo extends Component {
     this.handleToggle = this.handleToggle.bind(this);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log("TODO COMPONENT DID UPDATE");
+    console.log(prevProps.task);
+    console.log(this.props.task);
+  }
+
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
--- 36.todo/src/TodoList.js
+++ 36.todo/src/TodoList.js
@@ -26,6 +26,12 @@ class TodoList extends Component {
     });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log("IN COMPONENT DID UPDATE");
+    console.log(prevState.todos);
+    console.log(this.state.todos);
+  }
+
   update(id, updatedTask) {
     const updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
