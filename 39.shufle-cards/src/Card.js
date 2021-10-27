cee78180a441e9b18d785977ca23405bf42d6b3c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-27 23:17:01 +0200

    Adding the Card Component

---
+++ 39.shufle-cards/src/Card.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return (
+      <img className="Card" src={this.props.image} alt={this.props.name} />
+    );
+  }
+}
+
+export default Card;
--- 39.shufle-cards/src/Deck.js
+++ 39.shufle-cards/src/Deck.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import Card from "./Card";
 import axios from "axios";
-const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
+const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
   constructor(props) {
@@ -8,21 +9,20 @@ class Deck extends Component {
     this.state = { deck: null, drawn: [] };
     this.getCard = this.getCard.bind(this);
   }
-
   async componentDidMount() {
     let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
     this.setState({ deck: deck.data });
   }
-
   async getCard() {
-    let id = this.state.deck.deck_id;
+    let deck_id = this.state.deck.deck_id;
     try {
-      let cardURL = `${API_BASE_URL}/${id}/draw`;
-      let cardRes = await axios.get(cardURL);
+      let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+      let cardRes = await axios.get(cardUrl);
       if (!cardRes.data.success) {
-        throw new Error("No cards remaining!");
+        throw new Error("No card remaining!");
       }
       let card = cardRes.data.cards[0];
+      console.log(cardRes.data);
       this.setState((st) => ({
         drawn: [
           ...st.drawn,
@@ -39,13 +39,16 @@ class Deck extends Component {
   }
 
   render() {
+    const cards = this.state.drawn.map((c) => (
+      <Card key={c.id} name={c.name} image={c.image} />
+    ));
     return (
       <div>
         <h1>Card Dealer</h1>
         <button onClick={this.getCard}>Get Card!</button>
+        {cards}
       </div>
     );
   }
 }
-
 export default Deck;
