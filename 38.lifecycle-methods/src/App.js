2d0c79988aef8002882152a1f1fa1154ce31af6f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-27 18:39:54 +0200

    Loading Data With Async Functions

--- 38.lifecycle-methods/src/App.js
+++ 38.lifecycle-methods/src/App.js
@@ -1,12 +1,16 @@
 import Timer from "./Timer";
 import ZenQuote from "./ZenQuote";
+import GithubUserInfo from "./GithubUserInfo";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ZenQuote />
+      <GithubUserInfo username="facebook" />
+      <GithubUserInfo username="colt" />
+      <GithubUserInfo username="gaearon" />
+      <GithubUserInfo username="sophiebits" />
     </div>
   );
 }
---
+++ 38.lifecycle-methods/src/GithubUserInfo.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class GithubUserInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { imgUrl: "", name: "" };
+  }
+
+  async componentDidMount() {
+    const url = `https://api.github.com/users/${this.props.username}`;
+    let response = await axios.get(url);
+    let data = response.data;
+    this.setState({ imgUrl: data.avatar_url, name: data.name });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Github User: {this.state.name}</h1>
+        <img src={this.state.imgUrl} />
+      </div>
+    );
+  }
+}
+
+export default GithubUserInfo;
