c0c8535c39f2aa317b19072b67b9f81e8f804df1
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-27 13:47:35 +0200

    Final Touches and Bug Fixes

--- 37.yahtzee/src/ScoreTable.js
+++ 37.yahtzee/src/ScoreTable.js
@@ -19,6 +19,15 @@ import {
 import "./ScoreTable.css";
 
 class ScoreTable extends Component {
+  getTotalScore() {
+    const { scores } = this.props;
+    let totalScore = 0;
+    for (let key in scores) {
+      if (scores[key]) totalScore += scores[key];
+    }
+    return totalScore;
+  }
+
   render() {
     const { scores, doScore } = this.props;
 
@@ -128,6 +137,7 @@ class ScoreTable extends Component {
             </tbody>
           </table>
         </section>
+        <h2>TOTAL SCORE: {this.getTotalScore()}</h2>
       </div>
     );
   }
