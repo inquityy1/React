680af7febaf7654dc5ee9dba0c7007289560e516
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 19:19:02 +0200

    Switch and exact

--- 41.routing/src/App.js
+++ 41.routing/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import About from "./About";
-import { Route } from "react-router-dom";
+import Dog from "./Dog";
+import Contact from "./Contact";
 
 import "./App.css";
+
+const Hater = () => <h1>I ABSOLITELY HATE DOGS</h1>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/about" component={About} />
+        <Switch>
+          <Route path="/" exact component={About} />
+          <Route path="/dog" exact component={Dog} />
+          <Route path="/dog/hater" exact component={Hater} />
+          <Route path="/Contact" exact component={Contact} />
+        </Switch>
       </div>
     );
   }
