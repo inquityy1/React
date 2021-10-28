cca42bc0434b3772e59c78dd49008871857c981b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 21:44:38 +0200

    Routes

--- 42.vending-machine/src/App.js
+++ 42.vending-machine/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import VendingMachine from "./VendingMachine";
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" render={() => <VendingMachine />} />
+        <Route exact path="/chips" render={() => <Chips />} />
+        <Route exact path="/sardines" render={() => <Sardines />} />
+        <Route exact path="/soda" render={() => <Soda />} />
+      </Switch>
     </div>
   );
 }
---
+++ 42.vending-machine/src/VendingMachine.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class VendingMachine extends Component {
+  render() {
+    return (
+      <div>
+        <h1>VendingMachine</h1>
+      </div>
+    );
+  }
+}
