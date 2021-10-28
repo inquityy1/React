0f1684f99ccb44024b15101ba79b735ded631d18
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 19:23:31 +0200

    Link

--- 41.routing/src/App.js
+++ 41.routing/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -12,6 +12,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="App-nav">
+          <Link to="/dog">Dog</Link>
+          <Link to="/contact">contact</Link>
+          <Link to="/">About</Link>
+        </nav>
         <Switch>
           <Route path="/" exact component={About} />
           <Route path="/dog" exact component={Dog} />
