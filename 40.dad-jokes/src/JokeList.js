85d7561b40f3f403cc784e55a5ec59284ed3edf9
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 15:08:35 +0200

    Fixing Our LocalStorage Bug

--- 40.dad-jokes/src/JokeList.js
+++ 40.dad-jokes/src/JokeList.js
@@ -9,15 +9,19 @@ class JokeList extends Component {
   static defaultProps = {
     numJokesToGet: 10,
   };
+
   constructor(props) {
     super(props);
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
     };
+    this.handleClick = this.handleClick.bind(this);
   }
+
   componentDidMount() {
     if (this.state.jokes.length === 0) this.getJokes();
   }
+
   async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
@@ -26,16 +30,35 @@ class JokeList extends Component {
       });
       jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
     }
-    this.setState({ jokes: jokes });
-    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+    this.setState(
+      (st) => ({
+        jokes: [...st.jokes, ...jokes],
+      }),
+      () =>
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
   }
+
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+
   handleVote(id, delta) {
-    this.setState((st) => ({
-      jokes: st.jokes.map((j) =>
-        j.id === id ? { ...j, votes: j.votes + delta } : j
-      ),
-    }));
+    this.setState(
+      (st) => ({
+        jokes: st.jokes.map((j) =>
+          j.id === id ? { ...j, votes: j.votes + delta } : j
+        ),
+      }),
+      () =>
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
   }
+
+  handleClick() {
+    this.getJokes();
+  }
+
   render() {
     return (
       <div className="JokeList">
@@ -44,7 +67,9 @@ class JokeList extends Component {
             <span>Dad</span> Jokes
           </h1>
           <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
 
         <div className="JokeList-jokes">
