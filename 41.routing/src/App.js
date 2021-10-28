b7c0a252171e4db5a187eb55c94a0a1c0acb3ee1
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 19:34:20 +0200

    NavLink

--- 41.routing/src/App.js
+++ 41.routing/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -13,9 +13,15 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="App-nav">
-          <Link to="/dog">Dog</Link>
-          <Link to="/contact">contact</Link>
-          <Link to="/">About</Link>
+          <NavLink exact activeClassName="active-link" to="/dog">
+            Dog
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/contact">
+            contact
+          </NavLink>
+          <NavLink exact activeClassName="active-link" to="/">
+            About
+          </NavLink>
         </nav>
         <Switch>
           <Route path="/" exact component={About} />
