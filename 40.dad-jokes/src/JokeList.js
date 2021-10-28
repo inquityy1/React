a97df4ca9da72649cec787dfcf09cfe652caa2f0
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 15:43:15 +0200

    styling

--- 40.dad-jokes/src/JokeList.js
+++ 40.dad-jokes/src/JokeList.js
@@ -98,7 +98,7 @@ class JokeList extends Component {
           </h1>
           <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
           <button className="JokeList-getmore" onClick={this.handleClick}>
-            New Jokes
+            Fetch Jokes
           </button>
         </div>
 
