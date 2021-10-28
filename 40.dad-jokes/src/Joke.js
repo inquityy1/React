47060a8dcc556b81a0b8817d47a0a25be0f7c18f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 13:18:06 +0200

    Dynamic css for votes and emojis

--- 40.dad-jokes/src/Joke.js
+++ 40.dad-jokes/src/Joke.js
@@ -3,21 +3,53 @@ import React, { Component } from "react";
 import "./Joke.css";
 
 class Joke extends Component {
+  getColor() {
+    if (this.props.votes >= 15) {
+      return "#4CAF50";
+    } else if (this.props.votes >= 12) {
+      return "#CDDC39";
+    } else if (this.props.votes >= 9) {
+      return "#FFEB3B";
+    } else if (this.props.votes >= 6) {
+      return "#FFC107";
+    } else if (this.props.votes >= 3) {
+      return "#FF9800";
+    } else if (this.props.votes >= 0) {
+      return "#f44336";
+    }
+  }
+
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.votes >= 12) {
+      return "em em-laughing";
+    } else if (this.props.votes >= 9) {
+      return "em em-smiley";
+    } else if (this.props.votes >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.votes >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.votes >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  }
+
   render() {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-          <span className="Joke-votes">{this.props.votes}</span>
+          <span style={{ borderColor: this.getColor() }} className="Joke-votes">
+            {this.props.votes}
+          </span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
         </div>
         <div className="Joke-text">{this.props.text}</div>
         <div className="Joke-smiley">
-          <i
-            class="em em-rolling_on_the_floor_laughing"
-            aria-role="presentation"
-            aria-label="ROLLING ON THE FLOOR LAUGHING"
-          ></i>
+          <i className={this.getEmoji()}></i>
         </div>
       </div>
     );
