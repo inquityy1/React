d4f3eefff41113080acff95e125d60ec516a0890
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 12:53:54 +0200

    Styling

--- 40.dad-jokes/src/Joke.js
+++ 40.dad-jokes/src/Joke.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
+import "./Joke.css";
+
 class Joke extends Component {
   render() {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-          <span>{this.props.votes}</span>
+          <span className="Joke-votes">{this.props.votes}</span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
         </div>
         <div className="Joke-text">{this.props.text}</div>
+        <div className="Joke-smiley">
+          <i
+            class="em em-rolling_on_the_floor_laughing"
+            aria-role="presentation"
+            aria-label="ROLLING ON THE FLOOR LAUGHING"
+          ></i>
+        </div>
       </div>
     );
   }
