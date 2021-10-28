8fae1f5102828f141404b63d616fca26428863d3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 11:12:56 +0200

    Dad Jokes project

---
+++ 40.dad-jokes/src/App.js
@@ -0,0 +1,13 @@
+import JokeList from "./JokeList";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
+
+export default App;
---
+++ 40.dad-jokes/src/JokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class JokeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dad jokes</h1>
+      </div>
+    );
+  }
+}
+
+export default JokeList;
