9aa8606b6240234774faa2e192fe454e41a73976
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-28 22:01:00 +0200

    Navbar

--- 42.vending-machine/src/App.js
+++ 42.vending-machine/src/App.js
@@ -1,6 +1,7 @@
 import Chips from "./Chips";
 import Sardines from "./Sardines";
 import Soda from "./Soda";
+import Navbar from "./Navbar";
 import VendingMachine from "./VendingMachine";
 import { Route, Switch } from "react-router-dom";
 
@@ -9,6 +10,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" render={() => <VendingMachine />} />
         <Route exact path="/chips" render={() => <Chips />} />
---
+++ 42.vending-machine/src/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+import "./NavBar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <NavLink exact activeClassName="Navbar-active" to="/">
+          Home
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/chips">
+          Chips
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/soda">
+          Soda
+        </NavLink>
+        <NavLink exact activeClassName="Navbar-active" to="/sardines">
+          Sardines
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
