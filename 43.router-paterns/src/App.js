6911401c8361e85e40b2f44f07d520b939acec1c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-10-29 14:37:02 +0200

    404 error

--- 43.router-paterns/src/App.js
+++ 43.router-paterns/src/App.js
@@ -1,20 +1,26 @@
 import Food from "./Food";
 import Meal from "./Meal";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Route
-        exact
-        path="/food/:name"
-        render={(routeParams) => <Food authenticated={true} {...routeParams} />}
-      />
-      {/* <Route exact path="/food/:name" component={Food} /> - if u dont share props */}
+      <Switch>
+        <Route
+          exact
+          path="/food/:name"
+          render={(routeParams) => (
+            <Food authenticated={true} {...routeParams} />
+          )}
+        />
+        {/* <Route exact path="/food/:name" component={Food} /> - if u dont share props */}
 
-      <Route path="/food/:foodName/drink/:drinkName" component={Meal} />
+        <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+        <Route exact path="/" render={() => <h1>HOME PAGE</h1>} />
+        <Route render={() => <h1>ERROR NOT FOUND!!!</h1>} />
+      </Switch>
     </div>
   );
 }
