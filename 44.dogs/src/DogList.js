5ba42d11be62f136752c57ad19184fb31bb4d431
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-01 13:15:05 +0100

    A Couple Small Tweaks

--- 44.dogs/src/DogList.js
+++ 44.dogs/src/DogList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import "./DogList.css";
 
@@ -7,15 +8,16 @@ class DogList extends Component {
     return (
       <div className="DogList">
         <h1 className="display-1 text-center">Dog List</h1>
-        <div className="container">
-          <div className="row">
-            {this.props.dogs.map((d) => (
-              <div className="Dog col-lg-4 text-center" key={d.name}>
-                <img src={d.src} alt={d.name} />
-                <h3>{d.name}</h3>
-              </div>
-            ))}
-          </div>
+
+        <div className="row">
+          {this.props.dogs.map((d) => (
+            <div className="Dog col-lg-4 text-center" key={d.name}>
+              <img src={d.src} alt={d.name} />
+              <h3>
+                <Link to={`/dogs/${d.name}`}>{d.name}</Link>
+              </h3>
+            </div>
+          ))}
         </div>
       </div>
     );
