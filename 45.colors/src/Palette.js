25327c555ae3134fcac2d55b417e4deb14f38350
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-01 17:19:03 +0100

    Color box

--- 45.colors/src/Palette.js
+++ 45.colors/src/Palette.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+
+import "./Palette.css";
 
 class Pallete extends Component {
   render() {
+    const colorBoxes = this.props.colors.map((color) => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="Palette">
         {/* Navbar goes here */}
-        <div className="Palette-colors">{/* bunch of color boxes */}</div>
+        <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
       </div>
     );
