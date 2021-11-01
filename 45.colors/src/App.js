0664533d9c13ba0d810cba6e04e6788fd50e4767
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-01 19:21:46 +0100

    Generating Shades of Colors

--- 45.colors/src/App.js
+++ 45.colors/src/App.js
@@ -1,9 +1,11 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 import "./App.css";
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div className="App">
       <Palette {...seedColors[4]} />
