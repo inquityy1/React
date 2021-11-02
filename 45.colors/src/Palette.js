60cefbdaa10d465171411b17184fa43e17559c6a
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-02 10:33:08 +0100

    styling

--- 45.colors/src/Palette.js
+++ 45.colors/src/Palette.js
@@ -24,13 +24,15 @@ class Pallete extends Component {
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          onAfterChange={this.changeLevel}
-          step={100}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            onAfterChange={this.changeLevel}
+            step={100}
+          />
+        </div>
         {/* Navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
