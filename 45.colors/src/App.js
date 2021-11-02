210734ac335a7506e140082fc605f4d8a21c83f3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-02 17:50:42 +0100

    Palettes

--- 45.colors/src/App.js
+++ 45.colors/src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from "react";
 import Palette from "./Palette";
 import { Route, Switch } from "react-router-dom";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 import "./App.css";
+import Pallete from "./Palette";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-      <Route
-        exact
-        path="/palette/:id"
-        render={() => <h1>INDIVIDUAL PALETTE</h1>}
-      />
-    </Switch>
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
 
-    // <div className="App">
-    //   <Palette palette={generatePalette(seedColors[4])} />
-    // </div>
-  );
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
