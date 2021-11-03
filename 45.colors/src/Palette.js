fc5fd7f914ee8f4af5bd047b2c0736eef2decaae
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-03 17:23:50 +0100

    Move Out Styles

--- 45.colors/src/Palette.js
+++ 45.colors/src/Palette.js
@@ -3,19 +3,7 @@ import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
 import { withStyles } from "@material-ui/styles";
-
-import "./Palette.css";
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
-  colors: {
-    height: "90%",
-  },
-};
+import styles from "./styles/PaletteStyles";
 
 class Pallete extends Component {
   constructor(props) {
--- 45.colors/src/SingleColorPalette.js
+++ 45.colors/src/SingleColorPalette.js
@@ -4,48 +4,7 @@ import ColorBox from "./ColorBox";
 import PaletteFooter from "./PaletteFooter";
 import Navbar from "./Navbar";
 import { withStyles } from "@material-ui/styles";
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
-  colors: {
-    height: "90%",
-  },
-  goBack: {
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    opacity: 1,
-    backgroundColor: "black",
-    "& a": {
-      color: "white",
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      display: "inline-block",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15",
-      textAlign: "center",
-      outline: "none",
-      background: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      textTransform: "uppercase",
-      border: "none",
-      cursor: "pointer",
-      textDecoration: "none",
-    },
-  },
-};
+import styles from "./styles/PaletteStyles";
 
 class SingleColorPalette extends Component {
   constructor(props) {
