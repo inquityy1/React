56e2e7184c267fdc10169ca74b3d07d96a40c791
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-09 18:44:46 +0100

    SVG

--- 45.colors/src/styles/PaletteListStyles.js
+++ 45.colors/src/styles/PaletteListStyles.js
@@ -1,12 +1,18 @@
 import sizes from "./sizes";
+import bg from "./bg.svg";
 
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    backgroundColor: "#1e8feb",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll",
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
