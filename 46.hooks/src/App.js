bbc22b750a677ff0cf7609f4cd8693ec0091ce3a
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-10 14:31:19 +0100

    fetch Data w useEffect Hook

--- 46.hooks/src/App.js
+++ 46.hooks/src/App.js
@@ -1,20 +1,24 @@
-import CounterClass from "./CounterClass";
-import CounterHooks from "./CounterHooks";
-import Toggler from "./Toggler";
-import SimpleFormClass from "./SimpleFormClass";
-import SimpleFormHooks from "./SimpleFormHooks";
-import SimpleFormInputHook from "./SimpleFormInputHook";
+// import CounterClass from "./CounterClass";
+// import CounterHooks from "./CounterHooks";
+// import Toggler from "./Toggler";
+// import SimpleFormClass from "./SimpleFormClass";
+// import SimpleFormHooks from "./SimpleFormHooks";
+// import SimpleFormInputHook from "./SimpleFormInputHook";
+// import Clicker from "./Clicker";
+import SWMovies from "./SWMovies";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <CounterClass />
+      {/* <CounterClass />
       <CounterHooks />
       <Toggler />
       <SimpleFormClass />
       <SimpleFormHooks />
-      <SimpleFormInputHook />
+      <SimpleFormInputHook /> */}
+      {/* <Clicker /> */}
+      <SWMovies />
     </div>
   );
 }
