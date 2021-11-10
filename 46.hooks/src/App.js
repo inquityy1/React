a4b6bf15ce0bdbd570249d08138bcc3d6ca19c07
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-10 11:06:35 +0100

    Hooks

---
+++ 46.hooks/src/App.js
@@ -0,0 +1,13 @@
+import CounterClass from "./CounterClass";
+import CounterHooks from "./CounterHooks";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterHooks />
+    </div>
+  );
+}
+
+export default App;
---
+++ 46.hooks/src/CounterHooks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CounterHooks() {
+  return (
+    <div>
+      <h1>The Count Is: </h1>
+      <button>Add 1</button>
+    </div>
+  );
+}
+
+export default CounterHooks;
