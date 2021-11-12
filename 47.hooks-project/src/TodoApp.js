f20b7f4393eb0208656d511b7bdc3e713aed93f5
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-12 12:33:08 +0100

    add starter code for hooks project

---
+++ 47.hooks-project/src/TodoApp.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
+
+function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Clean Fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Grow Bread", completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
+}
+
+export default TodoApp;
+
+// - TodoApp
+//  -TodoForm
+//  -TodoList
+//   -TodoItem
