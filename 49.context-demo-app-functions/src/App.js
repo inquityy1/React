1deceeb0cfdb707bb7ef32c25298a2f81374e2f3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-20 17:37:05 +0100

    hookify the rest of the app

--- 49.context-demo-app-functions/src/App.js
+++ 49.context-demo-app-functions/src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import Form from "./Form";
 import PageContent from "./PageContent";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <LanguageProvider>
-          <PageContent>
-            <Navbar />
-            <Form />
-          </PageContent>
-        </LanguageProvider>
-      </ThemeProvider>
-    );
-  }
+function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
