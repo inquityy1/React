a9db32434e6c0ea9a57ddf8c829e9b6f048e660f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-20 17:32:06 +0100

    context providers w custom hook

--- 49.context-demo-app-functions/src/contexts/ThemeContext.js
+++ 49.context-demo-app-functions/src/contexts/ThemeContext.js
@@ -1,25 +1,14 @@
-import React, { Component, createContext } from "react";
+import React, { createContext } from "react";
+import useToggle from "../hooks/useToggleState";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isDarkMode: false };
-    this.toggleTheme = this.toggleTheme.bind(this);
-  }
+export function ThemeProvider(props) {
+  const [isDarkMode, toggleTheme] = useToggle(false);
 
-  toggleTheme() {
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  }
-
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ ...this.state, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
