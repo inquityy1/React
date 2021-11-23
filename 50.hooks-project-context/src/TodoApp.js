4777e19520e613ab603fb6b07913e22e58e5097d
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-23 09:56:07 +0100

    add todos.context

--- 50.hooks-project-context/src/TodoApp.js
+++ 50.hooks-project-context/src/TodoApp.js
@@ -7,6 +7,7 @@ import Paper from "@material-ui/core/Paper";
 import Toolbar from "@material-ui/core/Toolbar";
 import AppBar from "@material-ui/core/AppBar";
 import Grid from "@material-ui/core/Grid";
+import { TodosProvider } from "./context/todos.context";
 
 function TodoApp() {
   const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]");
@@ -34,13 +35,15 @@ function TodoApp() {
       </AppBar>
       <Grid container justify="center" style={{ marginTop: "1rem" }}>
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo} />
-          <TodoList
-            todos={todos}
-            removeTodo={removeTodo}
-            toggleTodo={toggleTodo}
-            editTodo={editTodo}
-          />
+          <TodosProvider>
+            <TodoForm addTodo={addTodo} />
+            <TodoList
+              todos={todos}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+          </TodosProvider>
         </Grid>
       </Grid>
     </Paper>
---
+++ 50.hooks-project-context/src/context/todos.context.js
@@ -0,0 +1,20 @@
+//todos
+//all methods to interact w/ todos
+import React, { createContext } from "react";
+import useTodoState from "../hooks/useTodoState";
+
+const defaultTodos = [
+  { id: 1, task: "Mow the lawn using goats", completed: false },
+  { id: 2, task: "Release lady bugs into garden", completed: true },
+];
+
+export const TodosContext = createContext();
+
+export function TodosProvider(props) {
+  const todosStuff = useTodoState(defaultTodos);
+  return (
+    <TodosContext.Provider value={todosStuff}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+}
