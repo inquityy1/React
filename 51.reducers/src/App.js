2b827ab554f3ab0d1f4527c3800aa5615df11ea3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-23 14:51:34 +0100

    useReducer

--- 51.reducers/src/App.js
+++ 51.reducers/src/App.js
@@ -1,23 +1,29 @@
-import logo from "./logo.svg";
+import React, { useReducer } from "react";
 import "./App.css";
 
+function countReducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + action.amount };
+    case "DECREMENT":
+      return { count: state.count - action.amount };
+    case "RESET":
+      return { count: 0 };
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(countReducer, { count: 10 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "INCREMENT", amount: 1 })}>
+        Add 1
+      </button>
+      <button onClick={() => dispatch({ type: "DECREMENT", amount: 1 })}>
+        Subtract 1
+      </button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
     </div>
   );
 }
