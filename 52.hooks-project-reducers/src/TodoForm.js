2ffe4d5395822fbf0451bfeb2d10f18786ddf76a
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-11-23 16:03:46 +0100

    Define todo reducer

--- 52.hooks-project-reducers/src/TodoForm.js
+++ 52.hooks-project-reducers/src/TodoForm.js
@@ -6,14 +6,14 @@ import { TodosContext } from "./context/todos.context";
 
 function TodoForm() {
   const [value, handleChange, reset] = useInputState("");
-  const { addTodo } = useContext(TodosContext);
+  const { dispatch } = useContext(TodosContext);
   console.log("TODO FORM RENDER");
   return (
     <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          addTodo(value);
+          dispatch({ type: "ADD", task: value });
           reset();
         }}
       >
