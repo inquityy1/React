97b3e611f876da6fff66ed2122bd5d1353c7a7ef
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-01-05 15:14:48 +0100

    useState hook example-2

--- 55.all-hooks/src/UseState.js
+++ 55.all-hooks/src/UseState.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 
 function UseState() {
-  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState("Pedro");
 
-  const increment = () => {
-    setCounter(counter + 1);
+  let onChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
   };
 
   return (
     <div>
-      <button onClick={increment}>click me</button>
-      <h1>{counter}</h1>
+      <input placeholder="enter something..." onChange={onChange} />
+      {inputValue}
     </div>
   );
 }
