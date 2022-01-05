9966bd4160e9fa31a0cc3bfd2ee474105e2847cc
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-01-05 16:07:27 +0100

    Add useReducer hook solution

--- 55.all-hooks/src/UseReducer.js
+++ 55.all-hooks/src/UseReducer.js
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
-export default function UseReducer() {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1, showText: state.showText };
+    case "toggleShowText":
+      return { count: state.count, showText: !state.showText };
+    default:
+      return state;
+  }
+};
+
+function UseReducer() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button
         onClick={() => {
-          setCount(count + 1);
-          setShowText(!showText);
+          dispatch({ type: "INCREMENT" });
+          dispatch({ type: "toggleShowText" });
         }}
       >
         Click Here
       </button>
 
-      {showText && <p>This is a text</p>}
+      {state.showText && <p>This is a text</p>}
     </div>
   );
 }
+
+export default UseReducer;
