599c6d7bd0e6fe69c6a6ac1a69cf4d920ddbd9d4
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-01-05 19:29:06 +0100

    Add useRef example

--- 55.all-hooks/src/UseRef.js
+++ 55.all-hooks/src/UseRef.js
@@ -1,11 +1,19 @@
 import React, { useRef } from "react";
 
 function UseRef() {
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    console.log(inputRef.current.value);
+    inputRef.current.focus();
+    inputRef.current.value = "";
+  };
+
   return (
     <div>
       <h1>Pedro</h1>
-      <input type="text" placeholder="Ex..." />
-      <button>Change Name</button>
+      <input type="text" placeholder="Ex..." ref={inputRef} />
+      <button onClick={onClick}>Change Name</button>
     </div>
   );
 }
