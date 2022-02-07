c482d1e306876488a1448214531cb605cb31e501
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 16:44:14 +0100

    Add new hemisphere assignment

---
+++ 57.hemisphere/src/HemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HemisphereDisplay = () => {
+  return (
+    <div>
+      <p>Hey you are in Northern hemisphere!</p>
+    </div>
+  );
+};
+
+export default HemisphereDisplay;
