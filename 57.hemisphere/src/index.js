287ab05268f3b44d5b00486f02abc2f1bd81647e
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 17:02:33 +0100

    Add geolocation

--- 57.hemisphere/src/index.js
+++ 57.hemisphere/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
   return <div>Hello World!</div>;
 };
 
