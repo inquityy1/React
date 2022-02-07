0252aef2e39ca91f8c3c13159d9eaffa247da4f4
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 15:05:12 +0100

    Add UI

--- 56.warming-up/src/index.js
+++ 56.warming-up/src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from "react-dom";
 
 const App = function () {
   return (
-    <div>
-      <p>hello world</p>
+    <div className="ui comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="profile picture" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sarah
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:00</span>
+          </div>
+          <div className="text">it's amazing</div>
+        </div>
+      </div>
     </div>
   );
 };
