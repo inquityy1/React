d34f69fb0577fa2c22fa917590bb00e803bab768
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 15:17:44 +0100

    Add img in first comment

--- 56.warming-up/src/index.js
+++ 56.warming-up/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pic1 from "./imgs/picture-1.jpg";
+import pic2 from "./imgs/picture-2.jpg";
+import pic3 from "./imgs/picture-3.jpg";
 
 const App = function () {
   return (
     <div className="ui comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img src="" alt="profile picture" />
+          <img src={pic1} alt="profile picture" />
         </a>
         <div className="content">
           <a href="/" className="author">
