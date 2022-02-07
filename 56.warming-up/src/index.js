17121ebbb6763c9109fe5b48f3bc19d2b83011de
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 12:05:04 +0100

    Add JSX syntax fort className= and style={{}}

--- 56.warming-up/src/index.js
+++ 56.warming-up/src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = function () {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label htmlFor="name" className="label">
+        enter e-mail
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "red", color: "white" }}>Submit</button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
