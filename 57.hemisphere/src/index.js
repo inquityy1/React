9f1da8f5a986d5de7114450842f65f89bd8aa9b5
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 17:14:16 +0100

    Replace function based component with class based component

--- 57.hemisphere/src/index.js
+++ 57.hemisphere/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
-  return <div>Hello World!</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    );
+    return (
+      <div>
+        <p>You are in the northern hemisphere</p>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
