d3abdc0db97d22d4096e6cee08f6afbb427e3cb7
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-07 15:57:12 +0100

    Delete all hard coded stuff and add it with props

--- 56.warming-up/src/SingleComment.js
+++ 56.warming-up/src/SingleComment.js
@@ -1,21 +1,21 @@
 import React from "react";
-import profile1 from "./imgs/picture-1.jpg";
 
-const SingleComment = ({ pic }) => {
+const SingleComment = (props) => {
+  console.log(props);
   return (
     <div className="ui comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img src={pic} alt="profile picture" />
+          <img src={props.pic} alt="profile picture" />
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sarah
+            {props.name}
           </a>
           <div className="metadata">
-            <span className="date">Today at 5:00</span>
+            <span className="date">{props.date}</span>
           </div>
-          <div className="text">it's amazing</div>
+          <div className="text">{props.text}</div>
         </div>
       </div>
     </div>
--- 56.warming-up/src/index.js
+++ 56.warming-up/src/index.js
@@ -8,9 +8,24 @@ import SingleComment from "./SingleComment";
 const App = function () {
   return (
     <div className="ui comments">
-      <SingleComment pic={pic1} />
-      <SingleComment pic={pic2} />
-      <SingleComment pic={pic3} />
+      <SingleComment
+        name="Alex"
+        date="Today at 5:00PM"
+        text="It is amazing"
+        pic={pic1}
+      />
+      <SingleComment
+        name="Jack"
+        date="Today at 6:00PM"
+        text="Great job"
+        pic={pic2}
+      />
+      <SingleComment
+        name="Sarah"
+        date="Today at 7:00PM"
+        text="Thanks..."
+        pic={pic3}
+      />
     </div>
   );
 };
