87aa0fada3e813a68dcd9008eb4bea8a84bde474
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-08 11:52:00 +0100

    Add pictures

--- 57.hemisphere/src/HemisphereDisplay.js
+++ 57.hemisphere/src/HemisphereDisplay.js
@@ -1,11 +1,15 @@
 import React from "react";
+import pic1 from "./img/picture1.jpg";
+import pic2 from "./img/picture2.jpg";
 
 const HemisphereDisplay = ({ latitude }) => {
   const hemisphere =
     latitude > 0 ? "Northern hemisphere" : "Southern hemisphere";
+  const picture = latitude > 0 ? pic1 : pic2;
 
   return (
     <div>
+      <img src={picture} alt="" />
       <p>{hemisphere}</p>
     </div>
   );
