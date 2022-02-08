1a626f6b23fa657667901dac44b15c95e753b318
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-08 13:23:40 +0100

    Add event handler

--- 58.image-list/src/components/SearchInput.js
+++ 58.image-list/src/components/SearchInput.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class SearchInput extends React.Component {
+  onInputChange(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <div className="ui massive icon input">
-              <input type="text" placeholder="search..." />
+              <input
+                type="text"
+                placeholder="search..."
+                onChange={this.onInputChange}
+              />
               <i className="search icon"></i>
             </div>
           </div>
