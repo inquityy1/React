c65cd517a09095fa520cecadc57ab9a90ab66613
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-09 11:38:32 +0100

    Add images state and axios request

--- 58.image-list/src/components/App.js
+++ 58.image-list/src/components/App.js
@@ -3,17 +3,20 @@ import axios from "axios";
 import SearchInput from "./SearchInput";
 
 class App extends React.Component {
-  async onSearchSubmit(entry) {
+  state = { images: [] };
+
+  onSearchSubmit = async (entry) => {
     const response = await axios.get(
       `https://pixabay.com/api/?key=25625049-f30c5e7c55321c2ac75879595&q=${entry}&image_type=photo`
     );
-    console.log(response.data.hits);
-  }
+    this.setState({ images: response.data.hits });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        We have {this.state.images.length} images
       </div>
     );
   }
