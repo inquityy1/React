735c8d9a5a72847ab9fe7004ec7f019568b2744c
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-10 12:08:08 +0100

    Refactor card component with redux

--- 59.react-router/src/components/Card.js
+++ 59.react-router/src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Card extends Component {
   //   state = { user: "" };
@@ -10,15 +11,24 @@ class Card extends Component {
 
   render() {
     // const { user } = this.state;
+    // const { title, body } = this.props.card;
     return (
       <div
         style={{ marginTop: "80px" }}
         className="ui raised very padded text container segment"
       >
-        <h3 className="ui header">{/*{user}*/}</h3>
+        {<h3 className="ui header"> {/*{this.title}*/}</h3>}
+        <p>{/*{this.body}*/}</p>
       </div>
     );
   }
 }
 
-export default Card;
+const mapStateToProps = (state, ownProps) => {
+  let title = ownProps.user;
+  return {
+    card: state.cards.find((card) => card.title === title),
+  };
+};
+
+export default connect(mapStateToProps)(Card);
