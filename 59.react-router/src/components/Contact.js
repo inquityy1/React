4f28bbc12a616ee4a014ebc408d887dbb71f655c
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-10 11:50:44 +0100

    Add mapStateToProps and connect high order component

--- 59.react-router/src/components/Contact.js
+++ 59.react-router/src/components/Contact.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Modal from "./Modal";
+// import Modal from "./Modal";
+import { connect } from "react-redux";
 
-const Contact = (props) => {
+const Contact = ({ cards }) => {
   return (
     <div>
-      <Modal />
-      <div
-        style={{ marginTop: "80px" }}
-        className="ui raised very padded text container segment"
-      >
-        <Link to="/alex" className="ui header">
-          alex
-        </Link>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an
-        </p>
-      </div>
-      <div
+      {/* <Modal /> */}
+      {cards.map((card) => {
+        return (
+          <div
+            key={card.id}
+            style={{ marginTop: "80px" }}
+            className="ui raised very padded text container segment"
+          >
+            <Link to={`/${card.title}`} className="ui header">
+              {card.title}
+            </Link>
+            <p>{card.body}</p>
+          </div>
+        );
+      })}
+      {/* <div
         style={{ marginTop: "80px" }}
         className="ui raised very padded text container segment"
       >
@@ -31,9 +33,15 @@ const Contact = (props) => {
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
 
-export default Contact;
+const mapStateToProps = (state) => {
+  return {
+    cards: state.cards,
+  };
+};
+
+export default connect(mapStateToProps)(Contact);
--- 59.react-router/src/reducers/rootReduxer.js
+++ 59.react-router/src/reducers/rootReduxer.js
@@ -1,5 +1,21 @@
 const initialState = {
-  cards: [],
+  cards: [
+    {
+      id: 1,
+      title: "Alex",
+      body: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    },
+    {
+      id: 2,
+      title: "Wilma",
+      body: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    },
+    {
+      id: 3,
+      title: "Jhon",
+      body: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+    },
+  ],
 };
 
 const rootReducer = (state = initialState, action) => {
