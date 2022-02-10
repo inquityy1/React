6a0129807d42279dcd8f04a5f28ef0b4261f35d1
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-10 19:40:57 +0100

    Reach out the context state

--- 60.contextapi/src/components/Navbar.js
+++ 60.contextapi/src/components/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 class Navbar extends React.Component {
+  static contextType = ThemeContext;
+
   render() {
+    const { isDarkTheme, darkTheme, lightTheme } = this.context;
+    const theme = isDarkTheme ? darkTheme : lightTheme;
     return (
-      <nav>
-        <h2 className="ui centered header">Oak Academy</h2>
+      <nav
+        style={{
+          background: theme.background,
+          color: theme.text,
+          height: "120px",
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>Oak Academy</h2>
         <div className="ui three buttons">
           <button className="ui button">Overview</button>
           <button className="ui button">Contact</button>
