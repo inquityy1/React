402c41d3089c6de5aa47f5b19953d293a20fa8a7
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-10 11:31:54 +0100

    Add redux

--- 59.react-router/src/index.js
+++ 59.react-router/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReduxer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
