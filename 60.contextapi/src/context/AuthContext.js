c31e2e8d58f031480fee98e2e2b0a22a3d7fcaf4
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-12 16:30:15 +0100

    Update class AuthContext with function component

--- 60.contextapi/src/context/AuthContext.js
+++ 60.contextapi/src/context/AuthContext.js
@@ -1,26 +1,21 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-class AuthContextProvider extends React.Component {
-  state = {
-    isLoggedIn: true,
+const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const changeAuthStatus = () => {
+    setIsLoggedIn(!isLoggedIn);
   };
 
-  changeAuthStatus = () => {
-    const { isLoggedIn } = this.state;
-    this.setState({ isLoggedIn: !isLoggedIn });
-  };
-
-  render() {
-    return (
-      <AuthContext.Provider
-        value={{ ...this.state, changeAuthStatus: this.changeAuthStatus }}
-      >
-        {this.props.children}
-      </AuthContext.Provider>
-    );
-  }
-}
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, changeAuthStatus: changeAuthStatus }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContextProvider;
