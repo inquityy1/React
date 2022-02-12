3d9b246be89f5f9b13b6e03707f675f360b07d69
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-12 22:30:02 +0100

    Add reducer component structure

--- 60.contextapi/src/context/TodoListContext.js
+++ 60.contextapi/src/context/TodoListContext.js
@@ -1,17 +1,7 @@
 import React, { createContext, useReducer } from "react";
+import { todosReducer } from "../reducers/todosReducer";
 
 export const TodoListContext = createContext();
-
-const todosReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_TODO":
-      return [...state, { text: action.text, id: Math.random() }];
-    case "REMOVE_TODO":
-      return state.filter((todo) => todo.id !== Number(action.id));
-    default:
-      return state;
-  }
-};
 
 const TodoListContextProvider = ({ children }) => {
   const [todos, dispatch] = useReducer(todosReducer, [
