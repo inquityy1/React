536d2c60608ee24c6d94e25f05087f533be66598
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-12 10:33:06 +0100

    Add some starter code

---
+++ 61.hooks/src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Pay bills</li>
+        <li>Do y our homework</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
