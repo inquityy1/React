ca7edaa9ad825658d4c38c20d9a21d1328621ee8
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-14 11:16:13 +0100

    Add setInterval and learning basics of nodejs

---
+++ 62.node-js-tutorial/app.js
@@ -0,0 +1,13 @@
+// console.log("hello world");
+
+let count = 0;
+
+const timer = setInterval(() => {
+  count = count + 2;
+  console.log(`${count} seconds have passed`);
+  if (count > 6) {
+    clearInterval(timer);
+  }
+}, 2000);
+
+console.log(__dirname);
