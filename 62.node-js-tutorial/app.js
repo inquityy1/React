aca16a087805e58dfba2fafcefd4edf7c6623b43
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-14 11:18:10 +0100

    __dirname and __filename

--- 62.node-js-tutorial/app.js
+++ 62.node-js-tutorial/app.js
@@ -1,13 +1,14 @@
 // console.log("hello world");
 
-let count = 0;
+// let count = 0;
 
-const timer = setInterval(() => {
-  count = count + 2;
-  console.log(`${count} seconds have passed`);
-  if (count > 6) {
-    clearInterval(timer);
-  }
-}, 2000);
+// const timer = setInterval(() => {
+//   count = count + 2;
+//   console.log(`${count} seconds have passed`);
+//   if (count > 6) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
 
-console.log(__dirname);
+// console.log(__dirname);
+// console.log(__filename);
