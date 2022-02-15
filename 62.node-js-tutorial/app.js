ca4c0b28af378771440f5f8605925ffd71b726aa
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-15 12:04:19 +0100

    Routes

--- 62.node-js-tutorial/app.js
+++ 62.node-js-tutorial/app.js
@@ -109,15 +109,16 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-
-  const person = {
-    name: "alex",
-    email: "alex@email.co",
-    job: "designer",
-  };
-
-  res.end(JSON.stringify(person));
+  if (req.url === "/home" || req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/about.html").pipe(res);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/404.html").pipe(res);
+  }
 });
 
 server.listen(3000, "127.0.0.1");
