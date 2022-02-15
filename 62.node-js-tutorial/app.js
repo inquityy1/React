d07d3abdb556b580aab2c64eca24978c5ae9fd9a
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-15 11:22:24 +0100

    Pipe

--- 62.node-js-tutorial/app.js
+++ 62.node-js-tutorial/app.js
@@ -108,11 +108,12 @@
 const http = require("http");
 const fs = require("fs");
 
-const readStream = fs.createReadStream(__dirname + "/read-me.txt", "utf-8");
-const writeStream = fs.createWriteStream(__dirname + "/write-me.txt");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
 
-readStream.on("data", (chunk) => {
-  console.log("new data received");
-  console.log(chunk);
-  writeStream.write(chunk);
+  const readStream = fs.createReadStream(__dirname + "/read-me.txt", "utf-8");
+
+  readStream.pipe(res);
 });
+
+server.listen(3000, "127.0.0.1");
