49c35bb5a4b3fc75834d25c0b7e815ba9281a39e
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-15 11:45:11 +0100

    Serving HTML

--- 62.node-js-tutorial/app.js
+++ 62.node-js-tutorial/app.js
@@ -109,9 +109,9 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
 
-  const readStream = fs.createReadStream(__dirname + "/read-me.txt", "utf-8");
+  const readStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
 
   readStream.pipe(res);
 });
