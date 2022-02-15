8dc784e28ebc3b710f1d6114610134f79c16be8b
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-15 11:12:03 +0100

    Write stream

--- 62.node-js-tutorial/app.js
+++ 62.node-js-tutorial/app.js
@@ -109,8 +109,10 @@ const http = require("http");
 const fs = require("fs");
 
 const readStream = fs.createReadStream(__dirname + "/read-me.txt", "utf-8");
+const writeStream = fs.createWriteStream(__dirname + "/write-me.txt");
 
 readStream.on("data", (chunk) => {
   console.log("new data received");
   console.log(chunk);
+  writeStream.write(chunk);
 });
