a0d6346bc219754951e0fd57ec89e651d96b74ef
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-17 23:33:51 +0100

    Update records

--- 63.MongoProject/students/src/student.js
+++ 63.MongoProject/students/src/student.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const StudentSchema = new Schema({
   name: String,
+  studentNumber: Number,
 });
 
 const Student = mongoose.model("student", StudentSchema);
--- 63.MongoProject/students/test/update_test.js
+++ 63.MongoProject/students/test/update_test.js
@@ -3,21 +3,46 @@ const Student = require("../src/student");
 
 describe("Updating records", () => {
   let jason;
+  let jason2;
 
   beforeEach((done) => {
-    jason = new Student({ name: "Jason" });
+    jason = new Student({ name: "Jason", studentNumber: 2500 });
+    jason2 = new Student({ name: "Jason", studentNumber: 3500 });
+    jason2.save();
     jason.save().then(() => done());
+    console.log(jason);
+    console.log(jason2);
   });
 
-  it("set and save", (done) => {
-    console.log(jason);
-    jason.set("name", "Alex");
-    jason
-      .save()
-      .then(() => Student.find({}))
-      .then((students) => {
-        assert(students[0].name === "Alex");
-      });
-    done();
+  //   it("set and save", (done) => {
+  //     console.log(jason);
+  //     jason.set("name", "Alex");
+  //     jason
+  //       .save()
+  //       .then(() => Student.find({}))
+  //       .then((students) => {
+  //         assert(students[0].name === "Alex");
+  //         done();
+  //       });
+  //   });
+
+  //   it("Update one of the Jasons", async () => {
+  //     const student = await Student.updateOne(
+  //       { name: "Jason" },
+  //       { studentNumber: 3000 }
+  //     );
+  //     const res = await Student.find({});
+  //     assert(res[0].studentNumber === 3000);
+  //     console.log(res);
+  //   });
+
+  it("Update Jason", async () => {
+    const student = await Student.updateMany(
+      { name: "Jason" },
+      { studentNumber: 3000 }
+    );
+    const res = await Student.find({});
+    assert(res[0].studentNumber === 3000 && res[1].studentNumber === 3000);
+    console.log(res);
   });
 });
