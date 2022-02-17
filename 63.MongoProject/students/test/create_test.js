e590d95cf0640792b005fbc15c3f447f056f651b
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-17 13:28:36 +0100

    db

--- 63.MongoProject/students/test/create_test.js
+++ 63.MongoProject/students/test/create_test.js
@@ -1,7 +1,9 @@
 const assert = require("assert");
+const Student = require("../src/student");
 
 describe("Create the first data", () => {
   it("Save the student", () => {
-    assert(2 + 3 === 5);
+    const jason = new Student({ name: "Jason" });
+    jason.save();
   });
 });
