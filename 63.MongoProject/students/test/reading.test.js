65e4e4963388983245786c1d913737eb6bc7a8c4
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-17 22:44:49 +0100

    findOne()

--- 63.MongoProject/students/test/reading.test.js
+++ 63.MongoProject/students/test/reading.test.js
@@ -3,15 +3,26 @@ const Student = require("../src/student");
 
 describe("Read the data", () => {
   let jason;
+  let jason2;
 
   beforeEach((done) => {
     jason = new Student({ name: "Jason" });
-    jason.save().then(() => done());
+    jason2 = new Student({ name: "Jason" });
+    jason.save();
+    jason2.save().then(() => done());
+
+    console.log(jason);
+    console.log(jason2);
   });
 
-  it("Find all Jasons", async () => {
-    const students = await Student.find({ name: "Jason" });
+  //   it("Find all Jasons", async () => {
+  //     const students = await Student.find({ name: "Jason" });
+  //     console.log(students);
+  //     assert(students[0]._id.toString() !== jason._id.toString());
+  //   });
+  it("Find one of the Jasons", async () => {
+    const students = await Student.findOne({ _id: jason._id });
     console.log(students);
-    assert(students[0]._id.toString() !== jason._id.toString());
+    assert(students.name === "Jason");
   });
 });
