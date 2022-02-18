bfa5a4e96b7638d31c112727cc5fda1c2ce6809a
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-18 10:57:13 +0100

    Mongoose validation

--- 63.MongoProject/students/src/student.js
+++ 63.MongoProject/students/src/student.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const StudentSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
   studentNumber: Number,
   articleCount: Number,
   grade: Number,
--- 63.MongoProject/students/test/validation_test.js
+++ 63.MongoProject/students/test/validation_test.js
@@ -1,2 +1,12 @@
 const assert = require("assert");
 const Student = require("../src/student");
+
+describe("validation", () => {
+  it("name is required", () => {
+    const student = new Student({ name: undefined });
+    const result = student.validateSync();
+    console.log(result);
+    const { message } = result.errors.name;
+    assert(message === "Name is required");
+  });
+});
