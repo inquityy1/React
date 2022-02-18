cab3c396ab343f808f8fb16fdafed510fd4b24c1
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-18 12:41:22 +0100

    Add new subdocument

--- 63.MongoProject/students/test/subdocument_test.js
+++ 63.MongoProject/students/test/subdocument_test.js
@@ -2,16 +2,32 @@ const assert = require("assert");
 const Student = require("../src/student");
 
 describe("Subdocument", () => {
-  it("Creating a subdocument", (done) => {
-    const jason = new Student({
-      name: "Jason",
-      articles: [{ title: "JavaScript" }],
-    });
-    jason.save().then(() => {
-      Student.findOne({ name: "Jason" }).then((student) => {
-        assert(student.articles[0].title === "JavaScript");
+  //   it("Creating a subdocument", (done) => {
+  //     const jason = new Student({
+  //       name: "Jason",
+  //       articles: [{ title: "JavaScript" }],
+  //     });
+  //     jason.save().then(() => {
+  //       Student.findOne({ name: "Jason" }).then((student) => {
+  //         assert(student.articles[0].title === "JavaScript");
+  //         done();
+  //       });
+  //     });
+  //   });
+
+  it("Adding new record", (done) => {
+    const jason = new Student({ name: "Jason", articles: [] });
+    jason
+      .save()
+      .then(() => Student.findOne({ name: "Jason" }))
+      .then((student) => {
+        student.articles.push({ title: "MongoDB" });
+        return student.save();
+      })
+      .then(() => Student.findOne({ name: "Jason" }))
+      .then((student) => {
+        assert(student.articles[0].title === "MongoDB");
         done();
       });
-    });
   });
 });
