d6f0de5605ee0c3c8e1e8a7a88d2b957b7b8643a
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-18 12:47:08 +0100

    Remove subdocument

--- 63.MongoProject/students/test/subdocument_test.js
+++ 63.MongoProject/students/test/subdocument_test.js
@@ -15,18 +15,37 @@ describe("Subdocument", () => {
   //     });
   //   });
 
-  it("Adding new record", (done) => {
-    const jason = new Student({ name: "Jason", articles: [] });
+  //   it("Adding new record", (done) => {
+  //     const jason = new Student({ name: "Jason", articles: [] });
+  //     jason
+  //       .save()
+  //       .then(() => Student.findOne({ name: "Jason" }))
+  //       .then((student) => {
+  //         student.articles.push({ title: "MongoDB" });
+  //         return student.save();
+  //       })
+  //       .then(() => Student.findOne({ name: "Jason" }))
+  //       .then((student) => {
+  //         assert(student.articles[0].title === "MongoDB");
+  //         done();
+  //       });
+  //   });
+
+  it("Remove the records", (done) => {
+    const jason = new Student({
+      name: "Jason",
+      articles: [{ title: "React Native" }],
+    });
     jason
       .save()
       .then(() => Student.findOne({ name: "Jason" }))
       .then((student) => {
-        student.articles.push({ title: "MongoDB" });
+        student.articles[0].remove();
         return student.save();
       })
       .then(() => Student.findOne({ name: "Jason" }))
       .then((student) => {
-        assert(student.articles[0].title === "MongoDB");
+        assert(student.articles.length === 0);
         done();
       });
   });
