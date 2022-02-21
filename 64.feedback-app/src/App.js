5486562d7502cee9ea0a2374195a72caa26326b6
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-21 13:18:36 +0100

    Into to JSX

--- 64.feedback-app/src/App.js
+++ 64.feedback-app/src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+
 function App() {
-  return <h1>Hello from the app component</h1>;
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  );
 }
 
 export default App;
