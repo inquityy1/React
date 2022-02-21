8734edb8a689cad99ff7ddce93756b520e98acc9
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-21 13:28:48 +0100

    Dynamic values & list in JSX

--- 64.feedback-app/src/App.js
+++ 64.feedback-app/src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
 
 function App() {
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
+
   return (
     <div className="container">
-      <h1>My App</h1>
+      <h1>{title.toUpperCase}</h1>
+      <p>{body}</p>
+      {Math.random() * (5 + 5)}
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
