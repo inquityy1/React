19742c7d9c6f9a76e88f2d92e05e50dabfe00b7f
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-21 13:54:23 +0100

    Create my first component & props

--- 64.feedback-app/src/App.js
+++ 64.feedback-app/src/App.js
@@ -1,38 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  const loading = false;
-  const showComments = false;
-
-  if (loading) return <h1>Loading...</h1>;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header text={true} />
+      <div>
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
