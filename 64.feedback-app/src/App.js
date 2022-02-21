6483f4481cec832e05f606c847194917ab1ca956
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-21 14:22:09 +0100

    State & useState hook

--- 64.feedback-app/src/App.js
+++ 64.feedback-app/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
       <Header />
       <div>
-        <h1>My App</h1>
+        <FeedbackItem />
       </div>
     </>
   );
---
+++ 64.feedback-app/src/components/FeedbackItem.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function FeedbackItem() {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState("This is an example of a feedback item");
+
+  const handleClick = () => {
+    setRating((prev) => {
+      console.log(prev);
+      return prev + 1;
+    });
+  };
+
+  return (
+    <div className="card">
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
+      <button onClick={handleClick}>Click</button>
+    </div>
+  );
+}
+
+export default FeedbackItem;
