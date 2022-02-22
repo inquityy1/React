4779e4d5a3a1e53436824dae475cdbf311858487
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-22 12:44:01 +0100

    Edit feedback event

--- 64.feedback-app/src/context/FeedbackContext.js
+++ 64.feedback-app/src/context/FeedbackContext.js
@@ -21,20 +21,36 @@ export const FeedbackProvider = ({ children }) => {
       rating: 10,
     },
   ]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
+  // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  // Add feedback
   const addFeedback = (newFeedback) => {
-    newFeedback.id = +uuidv4();
+    newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
-    <FeedbackContext.Provider value={{ feedback, addFeedback, deleteFeedback }}>
+    <FeedbackContext.Provider
+      value={{ feedback, addFeedback, deleteFeedback, editFeedback }}
+    >
       {children}
     </FeedbackContext.Provider>
   );
