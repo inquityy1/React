75c28df68ab12d936693a7cb6c82afd65d3508d6
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-23 17:18:09 +0100

    Search users

--- 65.github-finder/src/components/users/UserSearch.jsx
+++ 65.github-finder/src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from "../../context/github/GithubContext";
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users } = useContext(GithubContext);
+  const { users, searchUsers } = useContext(GithubContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -16,7 +16,7 @@ function UserSearch() {
     if (text === "") {
       alert("Please enter something");
     } else {
-      // @todo - search users
+      searchUsers(text);
 
       setText("");
     }
