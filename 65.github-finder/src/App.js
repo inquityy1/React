8993c6c878d5b4ba53476d5d02ef34e77d6497b6
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-23 10:48:50 +0100

    navbar

--- 65.github-finder/src/App.js
+++ 65.github-finder/src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main>Hello World</main>
+      </div>
+    </Router>
   );
 }
 
