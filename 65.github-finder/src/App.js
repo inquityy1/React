0600c524f7766c289dea6b3c8c6693646a2c9ea4
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-23 11:31:17 +0100

    Add pages and routes

--- 65.github-finder/src/App.js
+++ 65.github-finder/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
@@ -9,7 +12,14 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
 
-        <main className="container mx-auto px-3 pb-12">Hello World</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/notfound" element={<NotFound />} />
+            <Route exact path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
