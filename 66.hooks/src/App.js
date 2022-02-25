bbc6d7b1741cd81480e2cc44a0a470416dfd9ac1
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-02-25 11:37:51 +0100

    useRef

---
+++ 66.hooks/src/App.js
@@ -0,0 +1,11 @@
+import UseRefExample1 from "./components/UseRefExample1";
+
+function App() {
+  return (
+    <div className="App">
+      <UseRefExample1 />
+    </div>
+  );
+}
+
+export default App;
