1a87fb3a2e0761c0d6f0eeb926afe5629cee5438
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-03-01 12:43:16 +0100

    Add routes

--- 67.house-marketplace/src/App.js
+++ 67.house-marketplace/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
-    <div>
-      <h1>My app</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
+    </>
   );
 }
 
